bb7648cf6fb385d59150ebe0a7049d072625cc9a
Meinte Boersma <meinte.boersma@gmail.com>
2024-07-09 13:19:36 +0200

    trigger CI on PR

---
+++ test/LionWeb-CSharp-Test/StopCI.cs
@@ -0,0 +1,29 @@
+// Copyright 2024 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb_CSharp_Test;
+
+[TestClass]
+public class StopCI
+{
+
+    [TestMethod]
+    public void stop_CI_from_publishing_to_NuGet_org()
+    {
+        Assert.Fail("should stop the CI from publishing to NuGet.Org");
+    }
+}
