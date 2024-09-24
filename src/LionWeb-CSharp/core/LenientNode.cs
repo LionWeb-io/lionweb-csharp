53dd158ba368b406465b3ecb6f02d9f9e09e9a02
Niko Stotz <niko@nikostotz.de>
2024-09-24 19:29:56 +0200

    added documentation

--- src/LionWeb-CSharp/core/LenientNode.cs
+++ src/LionWeb-CSharp/core/LenientNode.cs
@@ -21,11 +21,41 @@ using M2;
 using M3;
 using System.Collections;
 
+/// <summary>
+/// A LenientNode can handle any kind of <i>feature value</i>
+/// or <see cref="IReadableNode.GetAnnotations()">annotations</see>,
+/// even if the <i>feature</i> is unknown to the <see cref="GetClassifier">classifier</see>
+/// or the <i>value</i> does not adhere to the <i>feature</i>.
+///
+/// It strikes the following compromises:
+/// <list type="bullet">
+/// <item>
+/// <inheritdoc cref="SetInternal"/>
+/// </item>
+/// <item>
+/// <see cref="IWritableNode.AddAnnotations"/>, <see cref="IWritableNode.InsertAnnotations"/>,
+/// and <see cref="IWritableNode.RemoveAnnotations"/> accept any node, even if the node is not an annotation,
+/// or does not annotate the target node.
+/// </item>
+/// <item>
+/// <inheritdoc cref="GetInternal"/>
+/// </item>
+/// <item>
+/// <inheritdoc cref="CollectAllSetFeatures"/>
+/// </item>
+/// </list>
+/// </summary>
 public class LenientNode : NodeBase, INode
 {
-    private readonly Dictionary<Feature, object?> _features = new();
-    private Classifier? _classifier = null;
+    private readonly Dictionary<Feature, object?> _featureValues = new();
+    private Classifier? _classifier;
 
+    /// <summary>
+    /// Constructs a new node.
+    /// </summary>
+    /// <param name="id"><c>this</c> node's <see cref="IReadableNode.GetId">id</see>.</param>
+    /// <param name="classifier"><c>this</c> node's <see cref="IReadableNode.GetClassifier()">classifier</see>.</param>
+    /// <exception cref="InvalidIdException">If <paramref name="id"/> is not a <see cref="IReadableNode.GetId">valid identifier</see>.</exception>
     public LenientNode(string id, Classifier? classifier) : base(id)
     {
         _classifier = classifier;
@@ -35,12 +65,25 @@ public class LenientNode : NodeBase, INode
     public override Classifier GetClassifier() =>
         _classifier ?? throw new UnsupportedNodeTypeException(this, "classifier");
 
+    /// <summary>
+    /// Sets this node's <see cref="IReadableNode.GetClassifier">classifier</see>.
+    /// </summary>
+    /// <param name="classifier">The new classifier.</param>
     public void SetClassifier(Classifier? classifier) => _classifier = classifier;
 
-    /// <inheritdoc />
-    public override IEnumerable<Feature> CollectAllSetFeatures() => _features.Keys;
+    /// <summary>
+    /// <see cref="IReadableNode.CollectAllSetFeatures"/> returns all set features, even if the feature is unknown to
+    /// the target node's classifier, or the feature's value doesn't adhere to the feature's type.
+    /// </summary>
+    public override IEnumerable<Feature> CollectAllSetFeatures() => _featureValues.Keys;
 
-    /// <inheritdoc />
+    /// <summary>
+    /// <see cref="IReadableNode.Get"/> returns single values as-is, and any enumerable nodes as
+    /// <c>List&lt;INode&gt;</c> (for containments) or <c>List&gt;IReadableNode&lt;</c> (for references or properties).
+    /// Throws <see cref="UnsetFeatureException"/> for unset <i>required features</i>.
+    /// If set, returns feature values for features unknown to the target node's classifier.
+    /// If set, returns values that might not fit the feature's type.
+    /// </summary>
     protected override bool GetInternal(Feature? feature, out object? result)
     {
         if (base.GetInternal(feature, out result))
@@ -49,7 +92,7 @@ public class LenientNode : NodeBase, INode
         if (feature == null)
             return false;
 
-        if (_features.TryGetValue(feature, out result))
+        if (_featureValues.TryGetValue(feature, out result))
         {
             switch (feature)
             {
@@ -87,14 +130,17 @@ public class LenientNode : NodeBase, INode
         }
 
         if (_classifier != null && _classifier.AllFeatures().Contains(feature))
-        {
             return true;
-        }
 
         return false;
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    /// <see cref="IWritableNode.Set"/> never fails, it accepts any feature and value --
+    /// including features unknown to the target node's classifier, <c>null</c> for <i>required features</i>,
+    /// and all of <i>value types</i>, single nodes, and collection of nodes for any feature.
+    /// For containments, sets the target node as parent of the value, even if the value doesn't fit the containment's type. 
+    /// </summary>
     protected override bool SetInternal(Feature? feature, object? value)
     {
         if (feature == null)
@@ -105,12 +151,12 @@ public class LenientNode : NodeBase, INode
             return true;
         }
 
-        var oldValue = _features.TryGetValue(feature, out var old) ? old : null;
+        var oldValue = _featureValues.TryGetValue(feature, out var old) ? old : null;
 
         switch (value)
         {
             case null:
-                if (_features.Remove(feature))
+                if (_featureValues.Remove(feature))
                     return true;
                 if (_classifier != null && _classifier.AllFeatures().Contains(feature))
                     return true;
@@ -122,18 +168,19 @@ public class LenientNode : NodeBase, INode
                     RemoveExistingChildren(c, oldValue);
                     AttachChild(node);
                 }
-                _features[feature] = readableNode;
+
+                _featureValues[feature] = readableNode;
                 return true;
 
-            case string s:
-                _features[feature] = value;
+            case string:
+                _featureValues[feature] = value;
                 return true;
 
             case IEnumerable:
                 var readableNodes = M2Extensions.AsNodes<IReadableNode>(value).ToList();
-                if (!readableNodes.Any())
+                if (readableNodes.Count == 0)
                 {
-                    if (_features.Remove(feature))
+                    if (_featureValues.Remove(feature))
                         return true;
                     if (_classifier != null && _classifier.AllFeatures().Contains(feature))
                         return true;
@@ -149,10 +196,10 @@ public class LenientNode : NodeBase, INode
                         AttachChild(newChild);
                     }
 
-                    _features[feature] = newChildren.ToList();
+                    _featureValues[feature] = newChildren.ToList();
                 } else
                 {
-                    _features[feature] = readableNodes;
+                    _featureValues[feature] = readableNodes;
                 }
 
                 return true;
@@ -161,7 +208,7 @@ public class LenientNode : NodeBase, INode
                 if (!v.GetType().IsValueType)
                     return false;
 
-                _features[feature] = value;
+                _featureValues[feature] = value;
                 return true;
         }
     }
@@ -174,7 +221,7 @@ public class LenientNode : NodeBase, INode
                 SetParentNull(n);
                 return;
             case List<INode> oldList:
-                RemoveSelfParent(oldList?.ToList(), oldList, c);
+                RemoveSelfParent(oldList.ToList(), oldList, c);
                 return;
         }
     }
@@ -185,12 +232,12 @@ public class LenientNode : NodeBase, INode
         if (base.DetachChild(child))
             return true;
 
-        foreach (var (key, value) in _features.Where(pair => pair.Key is Containment))
+        foreach (var (key, value) in _featureValues.Where(pair => pair.Key is Containment))
         {
             switch (value)
             {
-                case IReadableNode readable:
-                    _features.Remove(key);
+                case IReadableNode:
+                    _featureValues.Remove(key);
                     return true;
                 case IList list:
                     int index = list.IndexOf(child);
@@ -199,7 +246,7 @@ public class LenientNode : NodeBase, INode
 
                     list.RemoveAt(index);
                     if (list.Count == 0)
-                        _features.Remove(key);
+                        _featureValues.Remove(key);
 
                     return true;
             }
@@ -210,11 +257,11 @@ public class LenientNode : NodeBase, INode
 
     /// <inheritdoc/>
     public override Containment? GetContainmentOf(INode child) =>
-        _features
+        _featureValues
             .Keys
             .OfType<Containment>()
             .FirstOrDefault(k => k
-                .AsNodes<INode>(_features[k])
+                .AsNodes<INode>(_featureValues[k])
                 .Contains(child));
 
     /// <inheritdoc />
