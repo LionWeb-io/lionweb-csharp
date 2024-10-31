34590dd3b3fad53446ae637546c08ed7ebfadf61
Erkan Diken <erkan@f1re.io>
2024-10-31 12:35:23 +0100

    Extend UnknownClassifierTests with deserialization healing tests

--- test/LionWeb-CSharp-Test/tests/serialization/deserialization/customHandler/UnknownClassifierTests.cs
+++ test/LionWeb-CSharp-Test/tests/serialization/deserialization/customHandler/UnknownClassifierTests.cs
@@ -23,28 +23,29 @@ using LionWeb.Core.M1;
 using LionWeb.Core.M3;
 using LionWeb.Core.Serialization;
 
+/// <summary>
+/// <see cref="IDeserializerHandler.UnknownClassifier"/>
+/// </summary>
 [TestClass]
 public class UnknownClassifierTests
 {
-    /// <summary>
-    /// <see cref="IDeserializerHandler.UnknownClassifier"/>
-    /// </summary>
-
-    #region unknown classifier
-
-    private class UnknownClassifierDeserializerHandler : DeserializerExceptionHandler
+    private class DeserializerHealingHandler<TResult>(Func<TResult> heal) : DeserializerExceptionHandler
+        where TResult : Classifier?
     {
         public bool Called { get; private set; }
 
         public override Classifier? UnknownClassifier(CompressedMetaPointer classifier, CompressedId id)
         {
             Called = true;
-            return null;
+            return heal();
         }
     }
 
+    /// <summary>
+    /// Does not heal, returns null.
+    /// </summary>
     [TestMethod]
-    public void unknown_classifier()
+    public void unknown_classifier_does_not_heal()
     {
         var serializationChunk = new SerializationChunk
         {
@@ -64,16 +65,103 @@ public class UnknownClassifierTests
             ]
         };
 
-        var unknownClassifierDeserializerHandler = new UnknownClassifierDeserializerHandler();
+        var deserializerHealingHandler = new DeserializerHealingHandler<Classifier?>(() => null);
         IDeserializer deserializer = new DeserializerBuilder()
-            .WithHandler(unknownClassifierDeserializerHandler)
+            .WithHandler(deserializerHealingHandler)
             .WithLanguage(ShapesLanguage.Instance)
             .Build();
 
-        deserializer.Deserialize(serializationChunk);
-        Assert.IsTrue(unknownClassifierDeserializerHandler.Called);
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.IsTrue(deserializerHealingHandler.Called);
+        Assert.AreEqual(0, deserializedNodes.Count);
     }
 
-    #endregion
-    
+    /// <summary>
+    /// Heals by replacing unknown classifier with a known classifier in the language  
+    ///
+    /// Test scenario:
+    /// Deserialize a node which is serialized with a different (possibly one of the previous)
+    /// version of the language
+    /// </summary>
+    [TestMethod]
+    public void unknown_classifier_heals_from_not_matching_language_versions()
+    {
+        var serializationChunk = new SerializationChunk
+        {
+            SerializationFormatVersion = ReleaseVersion.Current,
+            Languages = [new SerializedLanguageReference { Key = "key-Shapes", Version = "1" }],
+            Nodes =
+            [
+                new SerializedNode
+                {
+                    Id = "foo",
+                    Classifier = new MetaPointer("key-Shapes", "0", "key-Circle"),
+                    Properties = [],
+                    Containments = [],
+                    References = [],
+                    Annotations = [],
+                }
+            ]
+        };
+
+        Func<Classifier?> heal = () => ShapesLanguage.Instance.Circle;
+        var unknownClassifierDeserializerHealingHandler = new DeserializerHealingHandler<Classifier?>(heal);
+
+        IDeserializer deserializer = new DeserializerBuilder()
+            .WithHandler(unknownClassifierDeserializerHealingHandler)
+            .WithLanguage(ShapesLanguage.Instance)
+            .Build();
+
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.IsTrue(unknownClassifierDeserializerHealingHandler.Called);
+        Assert.AreEqual(1, deserializedNodes.Count);
+        Assert.AreSame(ShapesLanguage.Instance.Circle, deserializedNodes[0].GetClassifier());
+    }
+
+    /// <summary>
+    /// Heals by replacing unknown classifier with a classifier from another language
+    /// </summary>
+    [TestMethod]
+    public void unknown_classifier_heals_from_unknown_classifier_key()
+    {
+        var serializationChunk = new SerializationChunk
+        {
+            SerializationFormatVersion = ReleaseVersion.Current,
+            Languages = [new SerializedLanguageReference { Key = "key-Shapes", Version = "1" }],
+            Nodes =
+            [
+                new SerializedNode
+                {
+                    Id = "foo",
+                    Classifier = new MetaPointer("key-Shapes", "1", "key-unknown-classifier"),
+                    Properties = [],
+                    Containments = [],
+                    References = [],
+                    Annotations = [],
+                }
+            ]
+        };
+
+        DynamicLanguage dynamicLanguage =
+            new("id-XLang") { Key = "key-XLang", Name = "XLang", Version = "1" };
+        DynamicConcept dynamicConcept =
+            dynamicLanguage.Concept("id-XLang-concept", "key-unknown-classifier", "name-XLang-concept:");
+
+        Func<Classifier?> heal = () => dynamicLanguage.Entities.OfType<Classifier>()
+            .FirstOrDefault(classifier => classifier.Key == serializationChunk.Nodes[0].Classifier.Key);
+
+        var deserializerHealingHandler = new DeserializerHealingHandler<Classifier?>(heal);
+
+        IDeserializer deserializer = new DeserializerBuilder()
+            .WithUncompressedIds(true)
+            .WithHandler(deserializerHealingHandler)
+            .WithLanguage(ShapesLanguage.Instance)
+            .WithLanguage(dynamicLanguage)
+            .Build();
+
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.IsTrue(deserializerHealingHandler.Called);
+        Assert.AreEqual(1, deserializedNodes.Count);
+        Assert.AreSame(dynamicConcept, deserializedNodes[0].GetClassifier());
+    }
 }
