4c253d050f0e713d8c0d575f20f5c10ba595a266
Erkan Diken <erkan@f1re.io>
2024-11-04 11:07:40 +0100

    Add healing test to UnresolvableAnnotation

--- test/LionWeb-CSharp-Test/tests/serialization/deserialization/customHandler/UnresolvableAnnotationTests.cs
+++ test/LionWeb-CSharp-Test/tests/serialization/deserialization/customHandler/UnresolvableAnnotationTests.cs
@@ -22,29 +22,22 @@ using LionWeb.Core;
 using LionWeb.Core.M1;
 using LionWeb.Core.Serialization;
 
+/// <summary>
+/// Tests for <see cref="IDeserializerHandler.UnresolvableAnnotation"/>
+/// </summary>
 [TestClass]
 public class UnresolvableAnnotationTests
 {
-    /// <summary>
-    /// <see cref="IDeserializerHandler.UnresolvableAnnotation"/>
-    /// </summary>
-
-    #region unresolvable annotation
-
-    private class UnresolvableAnnotationDeserializerHandler : DeserializerExceptionHandler
+    private class DeserializerHealingHandler(Func<CompressedId, IWritableNode, IWritableNode?> heal)
+        : DeserializerExceptionHandler
     {
-        public bool Called { get; private set; }
-
-        public override IWritableNode? UnresolvableAnnotation(CompressedId annotationId, IWritableNode node)
-        {
-            Called = true;
-            return null;
-        }
+        public override IWritableNode? UnresolvableAnnotation(CompressedId annotationId, IWritableNode node) =>
+            heal(annotationId, node);
     }
 
 
     [TestMethod]
-    public void unresolvable_annotation()
+    public void unresolvable_annotation_does_not_heal()
     {
         var serializationChunk = new SerializationChunk
         {
@@ -67,15 +60,56 @@ public class UnresolvableAnnotationTests
             ]
         };
 
-        var unresolvableAnnotationDeserializerHandler = new UnresolvableAnnotationDeserializerHandler();
+        var deserializerHealingHandler = new DeserializerHealingHandler((id, node) => null);
         IDeserializer deserializer = new DeserializerBuilder()
-            .WithHandler(unresolvableAnnotationDeserializerHandler)
+            .WithHandler(deserializerHealingHandler)
             .WithLanguage(ShapesLanguage.Instance)
             .Build();
 
-        deserializer.Deserialize(serializationChunk);
-        Assert.IsTrue(unresolvableAnnotationDeserializerHandler.Called);
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.AreEqual(1, deserializedNodes.Count);
     }
 
-    #endregion
+    [TestMethod]
+    public void unresolvable_annotation_heals()
+    {
+        var serializationChunk = new SerializationChunk
+        {
+            SerializationFormatVersion = ReleaseVersion.Current,
+            Languages =
+            [
+                new SerializedLanguageReference { Key = "key-Shapes", Version = "1" }
+            ],
+            Nodes =
+            [
+                new SerializedNode
+                {
+                    Id = "foo",
+                    Classifier = new MetaPointer("key-Shapes", "1", "key-Circle"),
+                    Properties = [],
+                    Containments = [],
+                    References = [],
+                    Annotations = ["unresolvable-annotation"],
+                }
+            ]
+        };
+
+        var documentation = new Documentation("new-annotation")
+        {
+            Technical = true,
+            Text = "this is a doc"
+        };
+        
+        var deserializerHealingHandler = new DeserializerHealingHandler((id, node) => documentation);
+        IDeserializer deserializer = new DeserializerBuilder()
+            .WithHandler(deserializerHealingHandler)
+            .WithLanguage(ShapesLanguage.Instance)
+            .Build();
+
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.AreEqual(1, deserializedNodes.Count);
+        INode? annotation = deserializedNodes.OfType<Circle>().FirstOrDefault()?.GetAnnotations()[0];
+        Assert.AreSame(documentation, annotation);
+        Assert.AreSame(documentation.GetId(), annotation?.GetId());
+    }
 }
