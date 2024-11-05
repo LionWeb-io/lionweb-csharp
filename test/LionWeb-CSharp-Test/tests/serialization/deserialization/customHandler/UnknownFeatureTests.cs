e37d67acf272e9c08ccb696296b52324aa925fc2
Erkan Diken <erkan@f1re.io>
2024-11-05 13:31:49 +0100

    Add healing tests to UnknownFeature

--- test/LionWeb-CSharp-Test/tests/serialization/deserialization/customHandler/UnknownFeatureTests.cs
+++ test/LionWeb-CSharp-Test/tests/serialization/deserialization/customHandler/UnknownFeatureTests.cs
@@ -24,30 +24,22 @@ using LionWeb.Core.M1;
 using LionWeb.Core.M3;
 using LionWeb.Core.Serialization;
 
+/// <summary>
+/// Tests for <see cref="IDeserializerHandler.UnknownFeature{TFeature}"/>
+/// </summary>
 [TestClass]
 public class UnknownFeatureTests
 {
-    /// <summary>
-    /// <see cref="IDeserializerHandler.UnknownFeature{TFeature}"/>
-    /// </summary>
-
-    #region unknown feature
-
-    private class UnknownFeatureDeserializerHandler : DeserializerExceptionHandler
+    private class DeserializerHealingHandler(Func<CompressedMetaPointer, Classifier, IWritableNode, Feature?> heal)
+        : DeserializerExceptionHandler
     {
-        public bool Called { get; private set; }
-
         public override Feature? UnknownFeature<TFeature>(CompressedMetaPointer feature, Classifier classifier,
-            IWritableNode node)
-        {
-            Called = true;
-            return null;
-        }
+            IWritableNode node) => heal(feature, classifier, node);
     }
 
 
     [TestMethod]
-    public void unknown_containment()
+    public void unknown_containment_does_not_heal()
     {
         var serializationChunk = new SerializationChunk
         {
@@ -58,34 +50,97 @@ public class UnknownFeatureTests
                 new SerializedNode
                 {
                     Id = "foo",
-                    Classifier = new MetaPointer("key-Shapes", "1", "key-Geometry"),
+                    Classifier = new MetaPointer("key-Shapes", "1", "key-Circle"),
                     Properties = [],
                     Containments =
                     [
                         new SerializedContainment
                         {
                             Containment = new MetaPointer("key-Shapes", "1", "key-unknown-containment"),
-                            Children = []
+                            Children = ["child"]
                         }
                     ],
                     References = [],
                     Annotations = [],
-                }
+                },
+
+                new SerializedNode
+                {
+                    Id = "child",
+                    Classifier = new MetaPointer("key-Shapes", "1", "key-Coord"),
+                    Properties = [],
+                    Containments = [],
+                    References = [],
+                    Annotations = [],
+                },
             ]
         };
 
-        var unknownFeatureDeserializerHandler = new UnknownFeatureDeserializerHandler();
+        var deserializerHealingHandler = new DeserializerHealingHandler((pointer, classifier, arg3) => null);
         IDeserializer deserializer = new DeserializerBuilder()
-            .WithHandler(unknownFeatureDeserializerHandler)
+            .WithHandler(deserializerHealingHandler)
             .WithLanguage(ShapesLanguage.Instance)
             .Build();
 
-        deserializer.Deserialize(serializationChunk);
-        Assert.IsTrue(unknownFeatureDeserializerHandler.Called);
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.AreEqual(2, deserializedNodes.Count);
+        Assert.AreEqual(0, deserializedNodes.OfType<Circle>().FirstOrDefault()?.CollectAllSetFeatures()
+            .OfType<Containment>().Count());
     }
 
     [TestMethod]
-    public void unknown_property_with_assigned_value()
+    public void unknown_containment_heals()
+    {
+        var serializationChunk = new SerializationChunk
+        {
+            SerializationFormatVersion = ReleaseVersion.Current,
+            Languages = [new SerializedLanguageReference { Key = "key-Shapes", Version = "1" }],
+            Nodes =
+            [
+                new SerializedNode
+                {
+                    Id = "foo",
+                    Classifier = new MetaPointer("key-Shapes", "1", "key-Circle"),
+                    Properties = [],
+                    Containments =
+                    [
+                        new SerializedContainment
+                        {
+                            Containment = new MetaPointer("key-Shapes", "1", "key-unknown-containment"),
+                            Children = ["child"]
+                        }
+                    ],
+                    References = [],
+                    Annotations = [],
+                },
+
+                new SerializedNode
+                {
+                    Id = "child",
+                    Classifier = new MetaPointer("key-Shapes", "1", "key-Coord"),
+                    Properties = [],
+                    Containments = [],
+                    References = [],
+                    Annotations = [],
+                },
+            ]
+        };
+
+        var deserializerHealingHandler =
+            new DeserializerHealingHandler((pointer, classifier, arg3) => ShapesLanguage.Instance.Circle_center);
+        IDeserializer deserializer = new DeserializerBuilder()
+            .WithHandler(deserializerHealingHandler)
+            .WithLanguage(ShapesLanguage.Instance)
+            .Build();
+
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.AreEqual(1, deserializedNodes.Count);
+        Assert.AreEqual(1, deserializedNodes.OfType<Circle>().FirstOrDefault()?.CollectAllSetFeatures()
+            .OfType<Containment>().Count());
+    }
+
+    [TestMethod]
+    public void unknown_property_does_not_heal()
     {
         var serializationChunk = new SerializationChunk
         {
@@ -114,55 +169,20 @@ public class UnknownFeatureTests
             ]
         };
 
-        var unknownFeatureDeserializerHandler = new UnknownFeatureDeserializerHandler();
+        var deserializerHealingHandler = new DeserializerHealingHandler((pointer, classifier, arg3) => null);
         IDeserializer deserializer = new DeserializerBuilder()
-            .WithHandler(unknownFeatureDeserializerHandler)
+            .WithHandler(deserializerHealingHandler)
             .WithLanguage(ShapesLanguage.Instance)
             .Build();
 
-        deserializer.Deserialize(serializationChunk);
-        Assert.IsTrue(unknownFeatureDeserializerHandler.Called);
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.AreEqual(1, deserializedNodes.Count);
+        Assert.AreEqual(0, deserializedNodes.OfType<Coord>().FirstOrDefault()?.CollectAllSetFeatures()
+            .OfType<Property>().Count());
     }
 
     [TestMethod]
-    public void unknown_property_without_assigned_value()
-    {
-        var serializationChunk = new SerializationChunk
-        {
-            SerializationFormatVersion = ReleaseVersion.Current,
-            Languages =
-            [
-                new SerializedLanguageReference { Key = "key-Shapes", Version = "1" }
-            ],
-            Nodes =
-            [
-                new SerializedNode
-                {
-                    Id = "foo",
-                    Classifier = new MetaPointer("key-Shapes", "1", "key-Coord"),
-                    Properties =
-                    [
-                        new SerializedProperty { Property = new MetaPointer("key-Shapes", "1", "key-unknown-property") }
-                    ],
-                    Containments = [],
-                    References = [],
-                    Annotations = [],
-                }
-            ]
-        };
-
-        var unknownFeatureDeserializerHandler = new UnknownFeatureDeserializerHandler();
-        IDeserializer deserializer = new DeserializerBuilder()
-            .WithHandler(unknownFeatureDeserializerHandler)
-            .WithLanguage(ShapesLanguage.Instance)
-            .Build();
-
-        deserializer.Deserialize(serializationChunk);
-        Assert.IsTrue(unknownFeatureDeserializerHandler.Called);
-    }
-
-    [TestMethod]
-    public void unknown_property_with_null_value()
+    public void unknown_property_heals()
     {
         var serializationChunk = new SerializationChunk
         {
@@ -181,7 +201,7 @@ public class UnknownFeatureTests
                     [
                         new SerializedProperty
                         {
-                            Property = new MetaPointer("key-Shapes", "1", "key-unknown-property"), Value = null
+                            Property = new MetaPointer("key-Shapes", "1", "key-unknown-property"), Value = "1"
                         }
                     ],
                     Containments = [],
@@ -191,18 +211,21 @@ public class UnknownFeatureTests
             ]
         };
 
-        var unknownFeatureDeserializerHandler = new UnknownFeatureDeserializerHandler();
+        var deserializerHealingHandler =
+            new DeserializerHealingHandler((pointer, classifier, arg3) => ShapesLanguage.Instance.Coord_x);
         IDeserializer deserializer = new DeserializerBuilder()
-            .WithHandler(unknownFeatureDeserializerHandler)
+            .WithHandler(deserializerHealingHandler)
             .WithLanguage(ShapesLanguage.Instance)
             .Build();
 
-        deserializer.Deserialize(serializationChunk);
-        Assert.IsTrue(unknownFeatureDeserializerHandler.Called);
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.AreEqual(1, deserializedNodes.Count);
+        Assert.AreEqual(1, deserializedNodes.OfType<Coord>().FirstOrDefault()?.CollectAllSetFeatures()
+            .OfType<Property>().Count());
     }
-
+    
     [TestMethod]
-    public void unknown_reference()
+    public void unknown_reference_does_not_heal()
     {
         var serializationChunk = new SerializationChunk
         {
@@ -251,15 +274,78 @@ public class UnknownFeatureTests
             ]
         };
 
-        var unknownFeatureDeserializerHandler = new UnknownFeatureDeserializerHandler();
+        var deserializerHealingHandler = new DeserializerHealingHandler((pointer, classifier, arg3) => null);
         IDeserializer deserializer = new DeserializerBuilder()
-            .WithHandler(unknownFeatureDeserializerHandler)
+            .WithHandler(deserializerHealingHandler)
             .WithLanguage(LibraryLanguage.Instance)
             .Build();
 
-        deserializer.Deserialize(serializationChunk);
-        Assert.IsTrue(unknownFeatureDeserializerHandler.Called);
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.AreEqual(2, deserializedNodes.Count);
+        Assert.AreEqual(0,
+            deserializedNodes.OfType<Book>().FirstOrDefault()?.CollectAllSetFeatures().OfType<Reference>().Count());
     }
 
-    #endregion
+    [TestMethod]
+    public void unknown_reference_heals()
+    {
+        var serializationChunk = new SerializationChunk
+        {
+            SerializationFormatVersion = ReleaseVersion.Current,
+            Languages =
+            [
+                new SerializedLanguageReference { Key = "library", Version = "1" }
+            ],
+            Nodes =
+            [
+                new SerializedNode
+                {
+                    Id = "foo",
+                    Classifier = new MetaPointer("library", "1", "Book"),
+                    Properties = [],
+                    Containments = [],
+                    References =
+                    [
+                        new SerializedReference
+                        {
+                            Reference = new MetaPointer("library", "1", "key-unknown-reference"),
+                            Targets =
+                            [
+                                new SerializedReferenceTarget { Reference = "author_1", ResolveInfo = "author" },
+                            ]
+                        }
+                    ],
+                    Annotations = [],
+                },
+
+                new SerializedNode
+                {
+                    Id = "author_1",
+                    Classifier = new MetaPointer("library", "1", "Writer"),
+                    Properties =
+                    [
+                        new SerializedProperty
+                        {
+                            Property = new MetaPointer("library", "1", "library_Writer_name"), Value = "author-name"
+                        }
+                    ],
+                    Containments = [],
+                    References = [],
+                    Annotations = [],
+                },
+            ]
+        };
+
+        var deserializerHealingHandler =
+            new DeserializerHealingHandler((pointer, classifier, arg3) => LibraryLanguage.Instance.Book_author);
+        IDeserializer deserializer = new DeserializerBuilder()
+            .WithHandler(deserializerHealingHandler)
+            .WithLanguage(LibraryLanguage.Instance)
+            .Build();
+
+        List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
+        Assert.AreEqual(2, deserializedNodes.Count);
+        Assert.AreEqual(1,
+            deserializedNodes.OfType<Book>().FirstOrDefault()?.CollectAllSetFeatures().OfType<Reference>().Count());
+    }
 }
