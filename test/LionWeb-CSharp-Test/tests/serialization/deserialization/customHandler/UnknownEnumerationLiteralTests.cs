6bf1d24c47f6da4fa34b39c37c6becd1d43162f6
Erkan Diken <erkan@f1re.io>
2024-11-07 14:33:04 +0100

    Add healing test to UnknownEnumerationLiteral

--- test/LionWeb-CSharp-Test/tests/serialization/deserialization/customHandler/UnknownEnumerationLiteralTests.cs
+++ test/LionWeb-CSharp-Test/tests/serialization/deserialization/customHandler/UnknownEnumerationLiteralTests.cs
@@ -78,8 +78,8 @@ public class UnknownEnumerationLiteralTests
         List<IReadableNode> deserializedNodes = deserializer.Deserialize(serializationChunk);
         Assert.IsFalse(deserializedNodes.OfType<EnumHolder>().FirstOrDefault()?.CollectAllSetFeatures()
             .OfType<Property>().Contains(WithEnumLanguage.Instance.EnumHolder_enumValue));
-    }    
-    
+    }
+
     [TestMethod]
     public void unknown_enumeration_literal_heals()
     {
@@ -123,5 +123,50 @@ public class UnknownEnumerationLiteralTests
         Assert.IsTrue(deserializedNodes.OfType<EnumHolder>().FirstOrDefault()?.CollectAllSetFeatures()
             .OfType<Property>().Contains(WithEnumLanguage.Instance.EnumHolder_enumValue));
         Assert.AreEqual(myEnumLiteral, deserializedNodes.OfType<EnumHolder>().FirstOrDefault()?.EnumValue);
+    }
+
+    private enum ForTestPurpose
+    {
+        InvalidEnum = 1
+    }
+
+    [TestMethod]
+    public void unknown_enumeration_literal_tries_to_heal_to_invalid_value()
+    {
+        var serializationChunk = new SerializationChunk
+        {
+            SerializationFormatVersion = ReleaseVersion.Current,
+            Languages =
+            [
+                new SerializedLanguageReference { Key = "WithEnum", Version = "1" }
+            ],
+            Nodes =
+            [
+                new SerializedNode
+                {
+                    Id = "foo",
+                    Classifier = new MetaPointer("WithEnum", "1", "EnumHolder"),
+                    Properties =
+                    [
+                        new SerializedProperty
+                        {
+                            Property = new MetaPointer("WithEnum", "1", "enumValue"),
+                            Value = "unknown-enumeration-literal"
+                        }
+                    ],
+                    Containments = [],
+                    References = [],
+                    Annotations = [],
+                }
+            ]
+        };
+
+        var deserializerHealingHandler =
+            new DeserializerHealingHandler((s, enumeration, arg3, arg4) => ForTestPurpose.InvalidEnum);
+        IDeserializer deserializer = new DeserializerBuilder()
+            .WithHandler(deserializerHealingHandler)
+            .WithLanguage(WithEnumLanguage.Instance)
+            .Build();
+        Assert.ThrowsException<InvalidValueException>(() => deserializer.Deserialize(serializationChunk));
     }
 }
