4e501e24d6929211e3bf482c79bc8c4780f03c49
Niko Stotz <niko@nikostotz.de>
2025-01-10 14:48:55 +0100

    added fuzzing tests for ListComparer

--- test/LionWeb.Core.Test/Utilities/ListComparerFuzzingTests.cs
+++ test/LionWeb.Core.Test/Utilities/ListComparerFuzzingTests.cs
@@ -21,6 +21,34 @@ using Core.Utilities;
 using Serialization;
 
 [TestClass]
+public class ListComparerFuzzingTests : AbsoluteIndexListComparerTestsBase
+{
+    const int Tries = 1000;
+    private const int MaxLength = 15;
+
+    public static IEnumerable<object[]> TestData
+    {
+        get => Enumerable.Range(0, Tries).Select(i => new object[]
+        {
+            StringRandomizer.Random(new Random().Next(MaxLength)),
+            StringRandomizer.Random(new Random().Next(MaxLength)),
+        });
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(TestData))]
+    public void Fuzz(string left, string right)
+        => AssertCompare(left, right);
+
+    [TestMethod]
+    public void Ex0() => AssertCompare("JK", "wm3cKcpKyBnI");
+
+
+    protected internal override IListComparer<char> CreateComparer(string left, string right)
+        => new ListComparer<char>(left.ToList(), right.ToList());
+}
+
+[TestClass]
 public class StepwiseFuzzingTests : ListComparerTestsBase
 {
     const int Tries = 1000;
@@ -42,7 +70,6 @@ public class StepwiseFuzzingTests : ListComparerTestsBase
 
     [TestMethod]
     public void Ex0() => AssertCompare("bRaMC1I0P2tFS", "0RYG", []);
-    
     
     protected internal override IListComparer<char> CreateComparer(string left, string right)
         => new StepwiseListComparer<char>(left.ToList(), right.ToList());
