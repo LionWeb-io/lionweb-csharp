983855961c32b8fd3b0cb670688b16aff5094d4a
Niko Stotz <niko@nikostotz.de>
2025-01-10 14:36:13 +0100

    fixed existing tests

--- test/LionWeb.Core.Test/Utilities/MoveDetectorListComparerTests.cs
+++ test/LionWeb.Core.Test/Utilities/MoveDetectorListComparerTests.cs
@@ -18,10 +18,9 @@
 namespace LionWeb.Core.Test.Utilities;
 
 using Core.Utilities;
-using Languages.Generated.V2024_1.SDTLang;
 
 [TestClass]
-public class StepwiseListComparerTests : ListComparerTestsBase
+public class MoveDetectorListComparerTests : ListComparerTestsBase
 {
     [TestMethod]
     public void Empty() =>
@@ -35,182 +34,299 @@ public class StepwiseListComparerTests : ListComparerTestsBase
     public void LeftEmptyOne() =>
         AssertCompare(
             "",
-            "a"
+            "a",
+            [
+                new(Add, 'a', 0),
+            ]
         );
 
     [TestMethod]
     public void LeftEmptyTwo() =>
         AssertCompare(
             "",
-            "ab"
+            "ab",
+            [
+                new(Add, 'a', 0),
+                new(Add, 'b', 1),
+            ]
         );
 
     [TestMethod]
     public void DeleteEveryOther() =>
         AssertCompare(
             "aBcDefGhijKl",
-            "acefhijl"
+            "acefhijl",
+            [
+                new(Delete, 'B', 1),
+                new(Delete, 'D', 2),
+                new(Delete, 'G', 4),
+                new(Delete, 'K', 7),
+            ]
         );
 
     [TestMethod]
     public void AddEveryOther() =>
         AssertCompare(
             "acefhijl",
-            "aBcDefGhijKl"
+            "aBcDefGhijKl",
+            [
+                new(Add, 'B', 1),
+                new(Add, 'D', 3),
+                new(Add, 'G', 6),
+                new(Add, 'K', 10),
+            ]
         );
 
     [TestMethod]
     public void AddDeleteEveryOther() =>
         AssertCompare(
             "aBceFgi",
-            "acDegHi"
+            "acDegHi",
+            [
+                new(Delete, 'B', 1),
+                new(Move, 'e', 2, 3),
+                new(Delete, 'F', 2),
+                new(Add, 'D', 2),
+                new(Add, 'H', 5),
+            ]
         );
 
     [TestMethod]
     public void DeleteAddAdd() =>
         AssertCompare(
             "aBcegi",
-            "acDegHi"
+            "acDegHi",
+            [
+                new(Delete, 'B', 1),
+                new(Add, 'D', 2),
+                new(Add, 'H', 5),
+            ]
         );
 
     [TestMethod]
     public void AddDeleteAdd() =>
         AssertCompare(
             "abdeFghjk",
-            "abCdeghIjk"
+            "abCdeghIjk",
+            [
+                new(Add, 'C', 2),
+                new(Delete, 'F', 5),
+                new(Add, 'I', 7),
+            ]
         );
 
     [TestMethod]
     public void DeleteAddDelete() =>
         AssertCompare(
             "aBceFgi",
-            "acDegi"
+            "acDegi",
+            [
+                new(Delete, 'B', 1),
+                new(Move, 'e', 2, 3),
+                new(Delete, 'F', 2),
+                new(Add, 'D', 2),
+            ]
         );
 
     [TestMethod]
     public void AddDelete() =>
         AssertCompare(
             "abCdegh",
-            "abdeFgh"
+            "abdeFgh",
+            [
+                new(Delete, 'C', 2),
+                new(Add, 'F', 4),
+            ]
         );
 
     [TestMethod]
     public void AddMultipleThenMoveRight() =>
         AssertCompare(
             "acefGi",
-            "aBcDeGfHi"
+            "aBcDeGfHi",
+            [
+                new(Add, 'B', 1),
+                new(Add, 'D', 3),
+                new(Move, 'G', 6, 5),
+                new(Add, 'H', 7),
+            ]
         );
 
     [TestMethod]
     public void AddMultipleThenMoveRightJ() =>
         AssertCompare(
             "acefGi",
-            "aBcDeJfHi"
+            "aBcDeJfHi",
+            [
+                new(Add, 'B', 1),
+                new(Add, 'D', 3),
+                new(Add, 'J', 5),
+                new(Delete, 'G', 7),
+                new(Add, 'H', 7),
+            ]
         );
 
     [TestMethod]
     public void MoveEveryOtherForward() =>
         AssertCompare(
             "aBcDe",
-            "acBeD"
+            "acBeD",
+            [
+                new(Move, 'B', 1, 3),
+                new(Move, 'D', 2, 4),
+            ]
         );
 
     [TestMethod]
     public void MoveEveryOtherSwap() =>
         AssertCompare(
             "abcdXefgYhij",
-            "abcdYefgXhij"
+            "abcdYefgXhij",
+            [
+                new(Move, 'X', 4, 8),
+                new(Move, 'Y', 7, 4),
+            ]
         );
 
     [TestMethod]
     public void RightEmptyOne() =>
         AssertCompare(
             "a",
-            ""
+            "",
+            [
+                new(Delete, 'a', 0),
+            ]
         );
 
     [TestMethod]
     public void RightEmptyTwo() =>
         AssertCompare(
             "ab",
-            ""
+            "",
+            [
+                new(Delete, 'a', 0),
+                new(Delete, 'b', 0),
+            ]
         );
 
     [TestMethod]
     public void MoveOneFarRight() =>
         AssertCompare(
             "aBcdefghijkl",
-            "acdefghiBjkl"
+            "acdefghiBjkl",
+            [
+                new(Move, 'B', 1, 8),
+            ]
         );
 
     [TestMethod]
     public void MoveOneFarLeft() =>
         AssertCompare(
             "acdefghiBjkl",
-            "aBcdefghijkl"
+            "aBcdefghijkl",
+            [
+                new(Move, 'B', 8, 1),
+            ]
         );
 
     [TestMethod]
     public void MoveTwoFarRight() =>
         AssertCompare(
             "aBCdefghijkl",
-            "adefghiBCjkl"
+            "adefghiBCjkl",
+            [
+                new(Move, 'B', 1, 8),
+                new(Move, 'C', 1, 8),
+            ]
         );
 
     [TestMethod]
     public void MoveTwoFarLeft() =>
         AssertCompare(
             "adefghiBCjkl",
-            "aBCdefghijkl"
+            "aBCdefghijkl",
+            [
+                new(Move, 'B', 7, 1),
+                new(Move, 'C', 8, 2),
+            ]
         );
 
     [TestMethod]
     public void MoveTwoSeparatedFarRight() =>
         AssertCompare(
             "aBcDefghijkl",
-            "acefghiBDjkl"
+            "acefghiBDjkl",
+            [
+                new(Move, 'B', 1, 8),
+                new(Move, 'D', 2, 8),
+            ]
         );
 
     [TestMethod]
     public void MoveTwoSeparatedFarLeft() =>
         AssertCompare(
             "acefghiBDjkl",
-            "aBcDefghijkl"
+            "aBcDefghijkl",
+            [
+                new(Move, 'B', 7, 1),
+                new(Move, 'D', 8, 3),
+            ]
         );
 
     [TestMethod]
     public void MoveOneFarRightDeleteAfter() =>
         AssertCompare(
             "aBcdefghijKl",
-            "acdefghiBjl"
+            "acdefghiBjl",
+            [
+                new(Move, 'B', 1, 10),
+                new(Move, 'j', 8, 10),
+                new(Delete, 'K', 8),
+            ]
         );
 
     [TestMethod]
     public void MoveOneFarLeftDeleteAfter() =>
         AssertCompare(
             "acdEfghiBjkl",
-            "aBcdfghijkl"
+            "aBcdfghijkl",
+            [
+                new(Move, 'B', 8, 1),
+                new(Delete, 'E', 4),
+            ]
         );
 
     [TestMethod]
     public void SwapTwo() =>
         AssertCompare(
             "ab",
-            "ba"
+            "ba",
+            [
+                new(Move, 'a', 0, 1)
+            ]
         );
 
     [TestMethod]
     public void SwapTwoFar() =>
         AssertCompare(
             "abCdefgHijkl",
-            "abHdefgCijkl"
+            "abHdefgCijkl",
+            [
+                new(Move, 'C', 2, 7),
+                new(Move, 'H', 6, 2),
+            ]
         );
 
     [TestMethod]
     public void Hirschberg() =>
         AssertCompare(
             "AbcdEF",
-            "bcdA"
+            "bcdA",
+            [
+                new(Move, 'A', 0, 5),
+                new(Delete, 'E', 3),
+                new(Delete, 'F', 3),
+            ]
         );
 
     [TestMethod]
@@ -219,8 +335,7 @@ public class StepwiseListComparerTests : ListComparerTestsBase
             "a" + new string('x', 2000),
             new string('x', 2000) + "a",
             [
-                new(Delete, 'a', 0),
-                new(Add, 'a', 2000)
+                new(Move, 'a', 0, 2000)
             ]
         );
 
@@ -236,14 +351,24 @@ public class StepwiseListComparerTests : ListComparerTestsBase
     public void AddDeleteMove() =>
         AssertCompare(
             "abcDefgHij",
-            "abceKfgiDj"
+            "abceKfgiDj",
+            [
+                new(Move, 'D', 3, 8),
+                new(Add, 'K', 4),
+                new(Delete, 'H', 7),
+            ]
         );
 
     [TestMethod]
     public void MoveDeleteDelete() =>
         AssertCompare(
             "abcDeKfgHij",
-            "abcefgiDj"
+            "abcefgiDj",
+            [
+                new(Move, 'D', 3, 9),
+                new(Delete, 'K', 4),
+                new(Delete, 'H', 6),
+            ]
         );
 
     [TestMethod]
@@ -251,8 +376,11 @@ public class StepwiseListComparerTests : ListComparerTestsBase
     public void Replaced() =>
         AssertCompare(
             "aBc",
-            "aDc"
+            "aDc",
+            [
+                new(Replace, 'B', 1, right: 'D')
+            ]
         );
     protected internal override IListComparer<char> CreateComparer(string left, string right)
-        => new StepwiseListComparer<char>(left.ToList(), right.ToList());
+        => new MoveDetector<char>(new StepwiseListComparer<char>(left.ToList(), right.ToList()).Compare());
 }
