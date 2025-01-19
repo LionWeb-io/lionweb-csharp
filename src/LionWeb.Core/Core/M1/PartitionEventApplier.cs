1fd1c4e057975af108218b11a30cf76d142d3b0d
Niko Stotz <niko@nikostotz.de>
2025-01-19 16:34:14 +0100

    reproduce ChildAdded on different node via listener

---
+++ src/LionWeb.Core/Core/M1/PartitionEventApplier.cs
@@ -0,0 +1,108 @@
+﻿// Copyright 2025 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.M1;
+
+using M3;
+using System.Collections;
+using Utilities;
+
+public class PartitionEventApplier
+{
+    private readonly IPartitionInstance _localPartition;
+    private readonly Dictionary<NodeId, IReadableNode> _nodeById;
+
+    public PartitionEventApplier(IPartitionInstance localPartition,
+        Dictionary<NodeId, IReadableNode>? sharedNodeMap = null)
+    {
+        _localPartition = localPartition;
+        _nodeById = sharedNodeMap ?? new();
+        Init();
+    }
+
+    public void Subscribe(IPartitionListener listener)
+    {
+        listener.ChildAdded += (sender, args) =>
+            OnRemoteChildAdded(sender, args.Parent, args.NewChild, args.Containment, args.Index);
+    }
+
+    private void Init()
+    {
+        RegisterNode(_localPartition);
+        _localPartition.Listener.ChildAdded += (sender, args) =>
+            OnLocalChildAdded(sender, args.Parent, args.NewChild, args.Containment, args.Index);
+    }
+
+    private void RegisterNode(IReadableNode newNode)
+    {
+        foreach (var node in M1Extensions.Descendants(newNode, true, true))
+        {
+            _nodeById[node.GetId()] = node;
+        }
+    }
+
+    protected virtual INode? Lookup(NodeId remoteNodeId) =>
+        (INode?)_nodeById.GetValueOrDefault(remoteNodeId);
+
+    protected virtual INode Clone(INode remoteNode) =>
+        new SameIdCloner(remoteNode.Descendants(true, true)).Clone()[remoteNode];
+
+    protected class SameIdCloner : Cloner
+    {
+        public SameIdCloner(IEnumerable<INode> inputNodes) : base(inputNodes)
+        {
+        }
+
+        protected override string GetNewId(INode remoteNode) =>
+            remoteNode.GetId();
+    }
+
+    #region Remote
+
+    private void OnRemoteChildAdded(object? sender, IWritableNode parent, IWritableNode newChild,
+        Containment containment, Index index)
+    {
+        var localParent = Lookup(parent.GetId()) ?? throw new NullReferenceException();
+        var newChildNode = (INode)newChild;
+
+        var clone = Clone(newChildNode);
+
+        object newValue = clone;
+        if (containment.Multiple)
+        {
+            var existingChildren = localParent.Get(containment);
+            if (existingChildren is IList l)
+            {
+                var children = new List<IWritableNode>(l.Cast<IWritableNode>());
+                children.Insert(index, clone);
+                newValue = children;
+            }
+        }
+
+        localParent.Set(containment, newValue);
+    }
+
+    #endregion
+
+    #region Local
+
+    private void OnLocalChildAdded(object? sender, IWritableNode parent, IWritableNode newChild,
+        Containment containment, Index index) =>
+        RegisterNode(newChild);
+
+    #endregion
+}
---
+++ test/LionWeb.Core.Test/Listener/EventTests.cs
@@ -0,0 +1,151 @@
+﻿// Copyright 2025 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.Test.Listener;
+
+using Core.Utilities;
+using Languages.Generated.V2024_1.Shapes.M2;
+using M1;
+using Comparer = Core.Utilities.Comparer;
+
+[TestClass]
+public class EventTests
+{
+    #region ChildAdded
+
+    [TestMethod]
+    public void ChildAdded_Multiple_Only()
+    {
+        var node = new Geometry("a");
+        
+        var clone = new Geometry("a");
+
+        var applier = new PartitionEventApplier(clone);
+        applier.Subscribe(node.Listener);
+
+        var added = new Circle("added");
+        node.AddShapes([added]);
+        
+        AssertEquals([node], [clone]);
+        Assert.AreNotSame(added, clone.Shapes[0]);
+    }
+
+    [TestMethod]
+    public void ChildAdded_Multiple_First()
+    {
+        var node = new Geometry("a")
+        {
+            Shapes = [
+                new Line("l")
+            ]
+        };
+        
+        var clone = new Geometry("a")
+        {
+            Shapes = [
+                new Line("l")
+            ]
+        };
+
+        var applier = new PartitionEventApplier(clone);
+        applier.Subscribe(node.Listener);
+
+        var added = new Circle("added");
+        node.InsertShapes(0, [added]);
+        
+        AssertEquals([node], [clone]);
+        Assert.AreNotSame(added, clone.Shapes[0]);
+    }
+
+    [TestMethod]
+    public void ChildAdded_Multiple_Last()
+    {
+        var node = new Geometry("a")
+        {
+            Shapes = [
+                new Line("l")
+            ]
+        };
+        
+        var clone = new Geometry("a")
+        {
+            Shapes = [
+                new Line("l")
+            ]
+        };
+
+        var applier = new PartitionEventApplier(clone);
+        applier.Subscribe(node.Listener);
+
+        var added = new Circle("added");
+        node.InsertShapes(1, [added]);
+        
+        AssertEquals([node], [clone]);
+        Assert.AreNotSame(added, clone.Shapes[1]);
+    }
+
+    [TestMethod]
+    public void ChildAdded_Single()
+    {
+        var node = new Geometry("a");
+        
+        var clone = new Geometry("a");
+
+        var applier = new PartitionEventApplier(clone);
+        applier.Subscribe(node.Listener);
+
+        var added = new Documentation("added");
+        node.Documentation = added;
+        
+        AssertEquals([node], [clone]);
+        Assert.AreNotSame(added, clone.Documentation);
+    }
+
+    [TestMethod]
+    public void ChildAdded_Deep()
+    {
+        var node = new Geometry("a");
+        
+        var clone = new Geometry("a");
+
+        var applier = new PartitionEventApplier(clone);
+        applier.Subscribe(node.Listener);
+
+        var added = new Circle("added")
+        {
+            Center = new Coord("coord")
+            {
+                X = 1,
+                Y = 2,
+                Z = 3
+            }
+        };
+        node.AddShapes([added]);
+        
+        AssertEquals([node], [clone]);
+        Assert.AreNotSame(added, clone.Shapes[0]);
+    }
+
+    #endregion
+
+    private void AssertEquals(IEnumerable<INode?> expected, IEnumerable<INode?> actual)
+    {
+        List<IDifference> differences = new Comparer(expected.ToList(), actual.ToList()).Compare().ToList();
+        Assert.IsFalse(differences.Count != 0, differences.DescribeAll(new()));
+    }
+    
+}
