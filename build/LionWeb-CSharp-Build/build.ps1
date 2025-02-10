e6240ae1f29c2f8c07899c614a9068217700cc75
Meinte Boersma <meinte.boersma@gmail.com>
2025-02-10 12:47:21 +0100

    Fixed and cleaned up make-scripts

--- build/LionWeb-CSharp-Build/build.ps1
+++ build/LionWeb-CSharp-Build/build.ps1
@@ -3,16 +3,12 @@ if (-Not (Test-Path $LocalChunks)) {
     New-Item -ItemType Directory -Path $LocalChunks
 }
 
-$TestProject = "../../test/LionWeb-CSharp-Test";
-$GeneratedPath = $TestProject + "/languages/generated";
+$TestProject = "../../test/LionWeb.Core.Test";
+$GeneratedPath = $TestProject + "/Languages/Generated";
 Remove-Item $GeneratedPath -Recurse
 New-Item -ItemType Directory -Path $GeneratedPath
+New-Item -ItemType Directory -Path $GeneratedPath/V2023_1
+New-Item -ItemType Directory -Path $GeneratedPath/V2024_1
 
 dotnet run Generate.cs --no-dependencies --no-restore
 
-$ExtDefs = "chunks/externalDefs";
-$LocalDefs = "chunks/localDefs";
-$TestProjectDefs = $TestProject + "/languages/defChunks";
-
-Move-Item -Path $LocalDefs/shapes.json -Destination $TestProjectDefs/ -Force
-Copy-Item -Path $ExtDefs/with-enum.json -Destination $TestProjectDefs/
