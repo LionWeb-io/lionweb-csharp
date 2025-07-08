5a75e7e1c971de88e31ce741d020dadec59a4cb2
Niko Stotz <niko@nikostotz.de>
2025-07-09 00:49:37 +0200

    fixed handling of arrays in Serialization records

---
+++ src/LionWeb.Core/Core/Serialization/RecordArrayExtensions.cs
@@ -0,0 +1,77 @@
+﻿// Copyright 2025 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.Serialization;
+
+using System.Text;
+
+/// Helpers to properly deal with arrays in records
+public static class RecordArrayExtensions
+{
+    /// Deep-equals of two arrays.
+    public static bool ArrayEquals(this object[]? left, object[]? right)
+    {
+        if (left == null || right == null)
+        {
+            return left == right;
+        }
+
+        return left.SequenceEqual(right);
+    }
+
+    /// Deep hash code of an array.
+    public static void ArrayHashCode(this ref HashCode hashCode, object[]? arr)
+    {
+        if (arr == null)
+        {
+            return;
+        }
+
+        foreach (var item in arr)
+        {
+            hashCode.Add(item);
+        }
+    }
+
+    /// Appends <paramref name="arr"/> to <paramref name="builder"/>.
+    public static void ArrayPrintMembers(this StringBuilder builder, object[]? arr)
+    {
+        if (arr == null)
+        {
+            builder.Append("null");
+            return;
+        }
+
+        builder.Append('[');
+
+        bool first = true;
+        foreach (var item in arr)
+        {
+            if (first)
+            {
+                first = false;
+            } else
+            {
+                builder.Append(", ");
+            }
+
+            builder.Append(item);
+        }
+
+        builder.Append(']');
+    }
+}
--- src/LionWeb.Core/Core/Serialization/Serialization.cs
+++ src/LionWeb.Core/Core/Serialization/Serialization.cs
@@ -15,7 +15,10 @@
 // SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// ReSharper disable CoVariantArrayConversion
 namespace LionWeb.Core.Serialization;
+
+using System.Text;
 
 /// <summary>
 /// This type, together with all the types in this file, represent data structures
@@ -23,6 +26,56 @@ namespace LionWeb.Core.Serialization;
 /// </summary>
 public record SerializationChunk
 {
+    /// <inheritdoc />
+    public virtual bool Equals(SerializationChunk? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return string.Equals(SerializationFormatVersion, other.SerializationFormatVersion,
+                   StringComparison.InvariantCulture) &&
+               Languages.ArrayEquals(other.Languages) &&
+               Nodes.ArrayEquals(other.Nodes);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        
+        hashCode.Add(SerializationFormatVersion, StringComparer.InvariantCulture);
+        hashCode.ArrayHashCode(Languages);
+        hashCode.ArrayHashCode(Nodes);
+
+        return hashCode.ToHashCode();
+    }
+
+    protected virtual bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append(nameof(SerializationFormatVersion));
+        builder.Append(" = ");
+        builder.Append(SerializationFormatVersion);
+        builder.Append(", ");
+
+        builder.Append(nameof(Languages));
+        builder.Append(" = ");
+        builder.ArrayPrintMembers(Languages);
+        builder.Append(", ");
+
+        builder.Append(nameof(Nodes));
+        builder.Append(" = ");
+        builder.ArrayPrintMembers(Nodes);
+
+        return true;
+    }
+
     public required string SerializationFormatVersion { get; init; }
     public required SerializedLanguageReference[] Languages { get; init; }
     public required SerializedNode[] Nodes { get; init; }
@@ -36,6 +89,83 @@ public record SerializedLanguageReference
 
 public record SerializedNode
 {
+    /// <inheritdoc />
+    public virtual bool Equals(SerializedNode? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return string.Equals(Id, other.Id, StringComparison.InvariantCulture) &&
+               Classifier.Equals(other.Classifier) &&
+               Properties.ArrayEquals(other.Properties) &&
+               Containments.ArrayEquals(other.Containments) &&
+               References.ArrayEquals(other.References) &&
+               Annotations.ArrayEquals(other.Annotations) &&
+               string.Equals(Parent, other.Parent, StringComparison.InvariantCulture);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        
+        hashCode.Add(Id, StringComparer.InvariantCulture);
+        hashCode.Add(Classifier);
+        hashCode.ArrayHashCode(Properties);
+        hashCode.ArrayHashCode(Containments);
+        hashCode.ArrayHashCode(References);
+        hashCode.ArrayHashCode(Annotations);
+        hashCode.Add(Parent, StringComparer.InvariantCulture);
+        
+        return hashCode.ToHashCode();
+    }
+
+    protected virtual bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append(nameof(Id));
+        builder.Append(" = ");
+        builder.Append(Id);
+        builder.Append(", ");
+
+        builder.Append(nameof(Classifier));
+        builder.Append(" = ");
+        builder.Append(Classifier);
+        builder.Append(", ");
+
+        builder.Append(nameof(Properties));
+        builder.Append(" = ");
+        builder.ArrayPrintMembers(Properties);
+        builder.Append(", ");
+
+        builder.Append(nameof(Containments));
+        builder.Append(" = ");
+        builder.ArrayPrintMembers(Containments);
+        builder.Append(", ");
+
+        builder.Append(nameof(References));
+        builder.Append(" = ");
+        builder.ArrayPrintMembers(References);
+        builder.Append(", ");
+
+        builder.Append(nameof(Annotations));
+        builder.Append(" = ");
+        builder.ArrayPrintMembers(Annotations);
+        builder.Append(", ");
+
+        builder.Append(nameof(Parent));
+        builder.Append(" = ");
+        builder.Append(Parent);
+
+        return true;
+    }
+
     public required string Id { get; init; }
     public required MetaPointer Classifier { get; init; }
     public required SerializedProperty[] Properties { get; init; }
@@ -55,6 +185,48 @@ public record SerializedProperty
 
 public record SerializedContainment
 {
+    /// <inheritdoc />
+    public virtual bool Equals(SerializedContainment? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Containment.Equals(other.Containment) &&
+               Children.ArrayEquals(other.Children);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        
+        hashCode.Add(Containment);
+        hashCode.ArrayHashCode(Children);
+
+        return hashCode.ToHashCode();
+    }
+
+    protected virtual bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append(nameof(Containment));
+        builder.Append(" = ");
+        builder.Append(Containment);
+        builder.Append(", ");
+
+        builder.Append(nameof(Children));
+        builder.Append(" = ");
+        builder.ArrayPrintMembers(Children);
+        
+        return true;
+    }
+
     public required MetaPointer Containment { get; init; }
     public required string[] Children { get; init; }
 }
@@ -67,6 +239,48 @@ public record SerializedReferenceTarget
 
 public record SerializedReference
 {
+    /// <inheritdoc />
+    public virtual bool Equals(SerializedReference? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Reference.Equals(other.Reference) &&
+               Targets.ArrayEquals(other.Targets);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        
+        hashCode.Add(Reference);
+        hashCode.ArrayHashCode(Targets);
+
+        return hashCode.ToHashCode();
+    }
+
+    protected virtual bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append(nameof(Reference));
+        builder.Append(" = ");
+        builder.Append(Reference);
+        builder.Append(", ");
+
+        builder.Append(nameof(Targets));
+        builder.Append(" = ");
+        builder.ArrayPrintMembers(Targets);
+        
+        return true;
+    }
+
     public required MetaPointer Reference { get; init; }
     public required SerializedReferenceTarget[] Targets { get; init; }
 }
