fd2c9deba6fb9528191e3a0f2180bcc0bebf555e
Niko Stotz <niko@nikostotz.de>
2025-08-07 00:12:31 +0200

    wip

---
+++ src/LionWeb.Core/Core/NodeId.cs
@@ -0,0 +1,214 @@
+﻿// Copyright 2025 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core;
+
+using System.Text;
+using Utilities;
+
+public record struct NodeIdX
+{
+    private const byte _invalidChar = byte.MaxValue;
+
+    private readonly byte[] _value;
+    internal readonly string? _invalidId;
+
+    public NodeIdX(string nodeId)
+    {
+        if (!Encode(nodeId, out _value))
+            _invalidId = nodeId;
+    }
+
+    public override readonly int GetHashCode() =>
+        _invalidId?.GetHashCode() ?? _value.GetHashCode();
+
+    public override readonly string ToString() =>
+        _invalidId ?? Decode(_value);
+
+    public readonly bool Equals(NodeIdX other)
+    {
+        if (_invalidId != null || other._invalidId != null)
+            return ToString().Equals(other.ToString(), StringComparison.InvariantCulture);
+
+        return ByteExtensions.Equals(_invalidId, other._invalidId);
+    }
+
+    private readonly bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append(ToString());
+        return true;
+    }
+
+    #region Encode
+
+    private static bool Encode(string nodeId, out byte[] result)
+    {
+        var maxLength = nodeId.Length * 6 / 8;
+        if (maxLength == 0)
+        {
+            result = [];
+            return false;
+        }
+
+        result = new byte[maxLength + 4];
+
+        int currentStringIndex = 0;
+        int currentByteIndex = 0;
+
+        while (currentStringIndex < nodeId.Length)
+        {
+            if (!EncodeFourChars(nodeId.AsSpan(currentStringIndex, 4), result.AsSpan(currentByteIndex, 3)))
+            {
+                result = [];
+                return false;
+            }
+
+            currentStringIndex += 4;
+            currentByteIndex += 3;
+        }
+
+        return true;
+    }
+
+    internal static bool EncodeFourChars(ReadOnlySpan<char> chars, Span<byte> bytes)
+    {
+        if (
+            !Encode(chars[0], out var b0) ||
+            !Encode(chars[1], out var b1) ||
+            !Encode(chars[2], out var b2) ||
+            !Encode(chars[3], out var b3)
+        )
+            return false;
+
+        Console.WriteLine($"b0:{b0:b8} b1:{b1:b8} b2:{b2:b8} b3:{b3:b8}");
+        
+        byte b0To0 = (byte)(b0 << 2);
+        byte b1To0 = (byte)((b1 & 0b110000) >> 4);
+        byte to0 = (byte)(b0To0 | b1To0);
+        bytes[0] = to0;
+        Console.WriteLine($"b0To0:{b0To0:b8} b1To0:{b1To0:b8} to0:{to0:b8}");
+        
+        byte b1To1 = (byte)((b1 & 0b1111) << 4);
+        byte b2To1 = (byte)((b2 & 0b111100) >> 2);
+        byte to1 = (byte)(b1To1 | b2To1);
+        bytes[1] = to1;
+        Console.WriteLine($"b1To1:{b1To1:b8} b2To1:{b2To1:b8} to1:{to1:b8}");
+        
+        byte b2To2 = (byte)(b2 << 6);
+        byte b3To2 = (byte)(b3);
+        byte to2 = (byte)(b2To2 | b3To2);
+        bytes[2] = to2;
+        Console.WriteLine($"b2To2:{b2To2:b8} b3To2:{b3To2:b8} to2:{to2:b8}");
+
+        return true;
+    }
+
+    internal static bool Encode(char c, out byte value)
+    {
+        switch (c)
+        {
+            case >= '0' and <= '9':
+                value = (byte)(c - '0');
+                break;
+            case >= 'A' and <= 'Z':
+                value = (byte)(c - 'A' + 10);
+                break;
+            case >= 'a' and <= 'z':
+                value = (byte)(c - 'a' + 36);
+                break;
+            case '-':
+                value = 62;
+                break;
+            case '_':
+                value = 63;
+                break;
+            default:
+                value = _invalidChar;
+                return false;
+        }
+
+        return true;
+    }
+
+    #endregion
+
+    #region Decode
+
+    private static string Decode(byte[] bytes)
+    {
+        var maxLength = bytes.Length * 8 / 6;
+
+        char[] result = new char[maxLength];
+
+        int currentStringIndex = 0;
+        int currentByteIndex = 0;
+
+        while (currentByteIndex < bytes.Length)
+        {
+            DecodeThreeBytes(bytes.AsSpan(currentByteIndex, 3), result.AsSpan(currentStringIndex, 4));
+
+            currentStringIndex += 3;
+            currentByteIndex += 4;
+        }
+
+        return new string(result);
+    }
+
+    internal static void DecodeThreeBytes(ReadOnlySpan<byte> bytes, Span<char> chars)
+    {
+        byte t0 = bytes[0];
+        byte t1 = bytes[1];
+        byte t2 = bytes[2];
+
+        Console.WriteLine($"t0:{t0:b8} t1:{t1:b8}  t2:{t2:b8}");
+
+
+        byte c0 = (byte)(t0 & 0b111111);
+        chars[0] = Decode((byte)c0);
+        Console.WriteLine($"c0:{c0:b8}");
+
+        byte t0ToC1 = (byte)((t0 & 0b11) << 4);
+        byte t1ToC1 = (byte)((t1 & 0b11110000) >> 4);
+        var c1 = (byte)(t0ToC1 | t1ToC1);
+        chars[1] = Decode(c1);
+        Console.WriteLine($"t0ToC1:{t0ToC1:b8} t1ToC1:{t1ToC1:b8} c1:{c1:b8}");
+
+
+        byte t1ToC2 = (byte)((t1 & 0b1111) << 2);
+        byte t2ToC2 = (byte)((t2  & 0b11000000) >> 6);
+        byte c2 = (byte)(t1ToC2 | t2ToC2);
+        chars[2] = Decode(c2);
+        Console.WriteLine($"t1ToC2:{t1ToC2:b8} t2ToC2:{t2ToC2:b8} c2:{c2:b8}");
+
+        byte c3 = (byte)(t2 & 0b111111);
+        chars[3] = Decode((byte)c3);
+        Console.WriteLine($"c3:{c3:b8}");
+    }
+
+    internal static char Decode(byte b) =>
+        b switch
+        {
+            <= 9 => (char)(b + '0'),
+            <= 35 => (char)(b - 10 + 'A'),
+            <= 61 => (char)(b - 36 + 'a'),
+            62 => '-',
+            63 => '_',
+            _ => char.MaxValue
+        };
+
+    #endregion
+}
---
+++ test/LionWeb.Core.Test/Encode.cs
@@ -0,0 +1,150 @@
+﻿// Copyright 2025 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.Test;
+
+using System.Reflection;
+
+[TestClass]
+public class Encode
+{
+    private static readonly Dictionary<byte, char> _mapping = new Dictionary<byte, char>()
+    {
+        { 0b000000, '0' },
+        { 0b000001, '1' },
+        { 0b000010, '2' },
+        { 0b000011, '3' },
+        { 0b000100, '4' },
+        { 0b000101, '5' },
+        { 0b000110, '6' },
+        { 0b000111, '7' },
+        { 0b001000, '8' },
+        { 0b001001, '9' },
+        { 0b001010, 'A' },
+        { 0b001011, 'B' },
+        { 0b001100, 'C' },
+        { 0b001101, 'D' },
+        { 0b001110, 'E' },
+        { 0b001111, 'F' },
+        { 0b010000, 'G' },
+        { 0b010001, 'H' },
+        { 0b010010, 'I' },
+        { 0b010011, 'J' },
+        { 0b010100, 'K' },
+        { 0b010101, 'L' },
+        { 0b010110, 'M' },
+        { 0b010111, 'N' },
+        { 0b011000, 'O' },
+        { 0b011001, 'P' },
+        { 0b011010, 'Q' },
+        { 0b011011, 'R' },
+        { 0b011100, 'S' },
+        { 0b011101, 'T' },
+        { 0b011110, 'U' },
+        { 0b011111, 'V' },
+        { 0b100000, 'W' },
+        { 0b100001, 'X' },
+        { 0b100010, 'Y' },
+        { 0b100011, 'Z' },
+        { 0b100100, 'a' },
+        { 0b100101, 'b' },
+        { 0b100110, 'c' },
+        { 0b100111, 'd' },
+        { 0b101000, 'e' },
+        { 0b101001, 'f' },
+        { 0b101010, 'g' },
+        { 0b101011, 'h' },
+        { 0b101100, 'i' },
+        { 0b101101, 'j' },
+        { 0b101110, 'k' },
+        { 0b101111, 'l' },
+        { 0b110000, 'm' },
+        { 0b110001, 'n' },
+        { 0b110010, 'o' },
+        { 0b110011, 'p' },
+        { 0b110100, 'q' },
+        { 0b110101, 'r' },
+        { 0b110110, 's' },
+        { 0b110111, 't' },
+        { 0b111000, 'u' },
+        { 0b111001, 'v' },
+        { 0b111010, 'w' },
+        { 0b111011, 'x' },
+        { 0b111100, 'y' },
+        { 0b111101, 'z' },
+        { 0b111110, '-' },
+        { 0b111111, '_' }
+    };
+
+    public static IEnumerable<object[]> TestPair =>
+        _mapping.Select(pair => new object[] { pair.Key, pair.Value });
+
+    [TestMethod]
+    [DynamicData(nameof(TestPair))]
+    public void EncodeChar(byte b, char c)
+    {
+        NodeIdX.Encode(c, out var actual);
+        Assert.AreEqual(b, actual);
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(TestPair))]
+    public void DecodeByte(byte b, char c)
+    {
+        var actual = NodeIdX.Decode(b);
+        Assert.AreEqual(c, actual);
+    }
+    
+    public static IEnumerable<object[]> FourChars =>
+    [
+        [new byte[] { 0, 0, 0 }, new char[] { '0', '0', '0', '0' }],
+        [new byte[] { 0b000000_00, 0b1010_1001, 0b00_111111 }, new char[] { '0', 'A', 'a', '_' }],
+        [new byte[] { 0b111111_11, 0b1110_1111, 0b11_111110 }, new char[] { '_', '-', '_', '-' }],
+        [new byte[] { 0b111111_11, 0b1111_1111, 0b11_111111 }, new char[] { '_', '_', '_', '_' }],
+    ];
+    
+    public static string GetTestDisplayNames(MethodInfo methodInfo, object[] values)
+    {
+        var bytes = (byte[])values[0];
+        var chars = (char[])values[1];
+    
+        return new string(chars);
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(FourChars), DynamicDataDisplayName = nameof(GetTestDisplayNames))]
+    public void EncodeFourChars(byte[] bytes, char[] chars)
+    {
+        var actual = new byte[3];
+        NodeIdX.EncodeFourChars(chars.AsSpan(), actual.AsSpan());
+        Assert.AreEqual(bytes[0], actual[0]);
+        Assert.AreEqual(bytes[1], actual[1]);
+        Assert.AreEqual(bytes[2], actual[2]);
+    }
+    
+    [TestMethod]
+    [DynamicData(nameof(FourChars), DynamicDataDisplayName = nameof(GetTestDisplayNames))]
+    public void DecodeThreeBytes(byte[] bytes, char[] chars)
+    {
+        char[] actual = new char[4];
+        NodeIdX.DecodeThreeBytes(bytes.AsSpan(), actual.AsSpan());
+        Assert.AreEqual(chars[0], actual[0]);
+        Assert.AreEqual(chars[1], actual[1]);
+        Assert.AreEqual(chars[2], actual[2]);
+        Assert.AreEqual(chars[3], actual[3]);
+    }
+}
