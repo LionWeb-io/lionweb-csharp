a1a0a674d900ad4e5ec50f7472a71ea3cc08134b
Niko Stotz <niko@nikostotz.de>
2025-08-13 11:35:02 +0200

    replaced ICommand/IPublisher with INotificationHandler

---
+++ src/LionWeb.Core/Core/Notification/Forest/ForestNotificationHandler.cs
@@ -0,0 +1,31 @@
+// Copyright 2025 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.Notification.Forest;
+
+using Handler;
+
+/// Provides notifications for adding and deleting <see cref="IPartitionInstance">partitions</see>.
+/// Raises notifications for adding and deleting <see cref="IPartitionInstance">partitions</see>.
+public interface IForestNotificationHandler : INotificationHandler<IForestNotification>;
+
+/// Forwards all <see cref="ModelNotificationHandlerBase{TNotification}.Receive">received</see> notifications
+/// unchanged to <i>following</i> notification handler,
+/// and to EventHandlers <see cref="ModelNotificationHandlerBase{TNotification}.Subscribe{TSubscribedNotification}">subscribed</see>
+/// to specific notifications.
+public class ForestNotificationHandler(object? sender)
+    : ModelNotificationHandlerBase<IForestNotification>(sender), IForestNotificationHandler;
