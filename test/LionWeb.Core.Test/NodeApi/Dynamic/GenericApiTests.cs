72e5e7459eb4969e8ecc4ea162216c67086b8a27
Erkan Diken <erkan@f1re.io>
2025-10-02 20:23:14 +0200

    Containment-related add/insert/remove tests were relocated from the static `ContainmentTests_Multiple_Optional` class to the dynamic `GenericApiTests` class.

--- test/LionWeb.Core.Test/NodeApi/Dynamic/GenericApiTests.cs
+++ test/LionWeb.Core.Test/NodeApi/Dynamic/GenericApiTests.cs
@@ -19,11 +19,66 @@ namespace LionWeb.Core.Test.NodeApi.Dynamic;
 
 using Languages.Generated.V2024_1.Multi.M2;
 using Languages.Generated.V2024_1.Shapes.M2;
+using Languages.Generated.V2025_1.Circular.A;
 using M3;
 
 [TestClass]
 public class GenericApiTests: DynamicNodeTestsBase
 {
+    #region dynamic language add/insert/remove generic api
+
+    #region entities
+
+    [TestMethod]
+    public void AddEntities()
+    {
+        var lionWebVersion = LionWebVersions.v2024_1;
+        var lang = new DynamicLanguage("myDynLang", lionWebVersion);
+        
+        List<LanguageEntity> entities = [new DynamicConcept("myDynConcept1", lionWebVersion, lang)];
+        
+        lang.Add(lionWebVersion.LionCore.Language_entities, entities);
+        
+        Assert.AreEqual(1, lang.Entities.Count);
+       
+    }
+
+    
+    [TestMethod]
+    public void InsertEntities()
+    {
+        var lionWebVersion = LionWebVersions.v2024_1;
+        var lang = new DynamicLanguage("myDynLang", lionWebVersion);
+        
+        List<LanguageEntity> entities = [
+            new DynamicConcept("myDynConcept1", lionWebVersion, lang),
+            new DynamicConcept("myDynConcept2", lionWebVersion, lang),
+        ];
+        
+        lang.Insert(lionWebVersion.LionCore.Language_entities, 0, entities);
+        
+        Assert.AreEqual(2, lang.Entities.Count);
+    }
+
+    [TestMethod]
+    public void RemoveEntities()
+    {
+        var lionWebVersion = LionWebVersions.v2024_1;
+        var lang = new DynamicLanguage("myDynLang", lionWebVersion);
+        
+        List<LanguageEntity> entities = [
+            new DynamicConcept("myDynConcept1", lionWebVersion, lang),
+            new DynamicConcept("myDynConcept2", lionWebVersion, lang),
+        ];
+        
+        lang.Insert(lionWebVersion.LionCore.Language_entities, 0, entities);
+        lang.Remove(lionWebVersion.LionCore.Language_entities, entities);
+        
+        Assert.AreEqual(0, lang.Entities.Count);
+    }
+
+    #endregion
+    
     #region dependsOn
 
     [TestMethod]
@@ -47,12 +102,13 @@ public class GenericApiTests: DynamicNodeTestsBase
         List<Language> languages = [ShapesLanguage.Instance, MultiLanguage.Instance];
         
         lang.Insert(lionWebVersion.LionCore.Language_dependsOn, 0, languages);
+        lang.Insert(lionWebVersion.LionCore.Language_dependsOn, 2, [ALangLanguage.Instance]);
         
-        Assert.AreEqual(2, lang.DependsOn.Count);
+        Assert.AreEqual(3, lang.DependsOn.Count);
     }
 
       
-    [TestMethod]
+    [TestMethod] 
     public void RemoveDependsOn()
     {
         var lionWebVersion = LionWebVersions.v2024_1;
@@ -64,8 +120,102 @@ public class GenericApiTests: DynamicNodeTestsBase
         
         Assert.AreEqual(0, lang.DependsOn.Count);
     }
+
+    #endregion
+
+    #region containments add/insert/remove generic api
+    
+    #region add
+
+    [TestMethod]
+    public void Single_Add_generic_api()
+    {
+        var parent = new Geometry("g");
+        var line = new Line("line");
+        
+        parent.Add(ShapesLanguage.Instance.Geometry_shapes, [line]);
+        
+        Assert.AreSame(parent, line.GetParent());
+        Assert.IsTrue(parent.Shapes.Contains(line));
+    }
+    
+    [TestMethod]
+    public void Multiple_Add_generic_api()
+    {
+        var parent = new Geometry("g");
+        var line1 = new Line("line1");
+        var line2 = new Line("line2");
+        
+        parent.Add(ShapesLanguage.Instance.Geometry_shapes, [line1, line2]);
+        
+        Assert.AreEqual(2, parent.Shapes.Count);
+        Assert.AreSame(parent, line1.GetParent());
+        Assert.AreSame(parent, line2.GetParent());
+        Assert.IsTrue(parent.Shapes.Contains(line1));
+        Assert.IsTrue(parent.Shapes.Contains(line2));
+        
+    }
     
     #endregion
 
+    #region insert
 
+    [TestMethod]
+    public void Multiple_Insert_generic_api()
+    {
+        var parent = new Geometry("g");
+        var line1 = new Line("line1");
+        var line2 = new Line("line2");
+
+        parent.Insert(ShapesLanguage.Instance.Geometry_shapes, 0, [line1, line2]);
+
+        Assert.AreEqual(2, parent.Shapes.Count);
+        Assert.AreSame(parent, line1.GetParent());
+        Assert.AreSame(parent, line2.GetParent());
+        Assert.IsTrue(parent.Shapes.Contains(line1));
+        Assert.IsTrue(parent.Shapes.Contains(line2));
+    }
+
+    #endregion
+
+    #region remove
+
+    [TestMethod]
+    public void Single_Remove_generic_api()
+    {
+        var parent = new Geometry("g");
+        var line = new Line("myId");
+        
+        parent.Insert(ShapesLanguage.Instance.Geometry_shapes, 0, [line]);
+        parent.Remove(ShapesLanguage.Instance.Geometry_shapes, [line]);
+        
+        Assert.IsNull(line.GetParent());
+        Assert.IsFalse(parent.Shapes.Contains(line));
+        
+    }
+    
+    [TestMethod]
+    public void Multiple_Remove_generic_api()
+    {
+        var parent = new Geometry("g");
+        var line1 = new Line("line1");
+        var line2 = new Line("line2");
+        
+        parent.Insert(ShapesLanguage.Instance.Geometry_shapes, 0, [line1]);
+        parent.Insert(ShapesLanguage.Instance.Geometry_shapes, 0, [line2]);
+        parent.Remove(ShapesLanguage.Instance.Geometry_shapes, [line1, line2]);
+        
+        Assert.IsEmpty(parent.Shapes);
+        Assert.IsNull(line1.GetParent());
+        Assert.IsNull(line2.GetParent());
+        Assert.IsFalse(parent.Shapes.Contains(line1));
+        Assert.IsFalse(parent.Shapes.Contains(line2));
+    }
+    
+    #endregion
+
+    #endregion
+    
+    #endregion
+    
 }
