6637d3fb665b64b0c802d23522f191771806cc5e
Erkan Diken <erkan@f1re.io>
2025-10-03 13:45:34 +0200

    Add unit tests for annotation generic API methods
    This commit introduces unit tests for add, insert, and remove operations on annotations in the generic API. These tests validate various scenarios, including boundary conditions, multiple annotations, and edge cases, ensuring the correctness of the API functionalities.

--- test/LionWeb.Core.Test/NodeApi/Dynamic/GenericApiTests.cs
+++ test/LionWeb.Core.Test/NodeApi/Dynamic/GenericApiTests.cs
@@ -123,6 +123,8 @@ public class GenericApiTests : DynamicNodeTestsBase
 
     #endregion
 
+    #endregion
+    
     #region containments add/insert/remove generic api
 
     #region add
@@ -211,6 +213,217 @@ public class GenericApiTests : DynamicNodeTestsBase
     }
 
     #endregion
+
+    #endregion
+
+    #region annotations add/insert/remove generic api
+
+    #region Single
+
+    [TestMethod]
+    public void Single_Add()
+    {
+        var parent = newLine("g");
+        var bom = newBillOfMaterials("myId");
+        parent.Add(null, [bom]);
+        Assert.AreSame(parent, bom.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(bom));
+    }
+    
+    #region Insert
+
+    [TestMethod]
+    public void Single_Insert_Empty()
+    {
+        var parent = newLine("g");
+        var bom = newBillOfMaterials("myId");
+        parent.Insert(null, 0, [bom]);
+        Assert.AreSame(parent, bom.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(bom));
+    }
+
+    [TestMethod]
+    public void Single_Insert_Empty_UnderBounds()
+    {
+        var parent = newLine("g");
+        var bom = newBillOfMaterials("myId");
+        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => parent.Insert(null, -1, [bom]));
+        Assert.IsNull(bom.GetParent());
+        Assert.IsFalse(parent.GetAnnotations().Contains(bom));
+    }
+
+    [TestMethod]
+    public void Single_Insert_Empty_OverBounds()
+    {
+        var parent = newLine("g");
+        var bom = newBillOfMaterials("myId");
+        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => parent.Insert(null,1, [bom]));
+        Assert.IsNull(bom.GetParent());
+        Assert.IsFalse(parent.GetAnnotations().Contains(bom));
+    }
+
+    [TestMethod]
+    public void Single_Insert_One_Before()
+    {
+        var doc = newDocumentation("cId");
+        var parent = newLine("g");
+        parent.Add(null, [doc]);
+        var bom = newBillOfMaterials("myId");
+        parent.Insert(null, 0, [bom]);
+        Assert.AreSame(parent, doc.GetParent());
+        Assert.AreSame(parent, bom.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(bom));
+        CollectionAssert.AreEqual(new List<INode> { bom, doc }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void Single_Insert_One_After()
+    {
+        var doc = newDocumentation("cId");
+        var parent = newLine("g");
+        parent.Add(null, [doc]);
+        var bom = newBillOfMaterials("myId");
+        parent.Insert(null, 1, [bom]);
+        Assert.AreSame(parent, doc.GetParent());
+        Assert.AreSame(parent, bom.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(bom));
+        CollectionAssert.AreEqual(new List<INode> { doc, bom }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void Single_Insert_Two_Before()
+    {
+        var docA = newDocumentation("cIdA");
+        var docB = newDocumentation("cIdB");
+        var parent = newLine("g");
+        parent.Add(null, [docA, docB]);
+        var bom = newBillOfMaterials("myId");
+        parent.Insert(null, 0, [bom]);
+        Assert.AreSame(parent, docA.GetParent());
+        Assert.AreSame(parent, docB.GetParent());
+        Assert.AreSame(parent, bom.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(bom));
+        CollectionAssert.AreEqual(new List<INode> { bom, docA, docB }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void Single_Insert_Two_Between()
+    {
+        var docA = newDocumentation("cIdA");
+        var docB = newDocumentation("cIdB");
+        var parent = newLine("g");
+        parent.Add(null, [docA, docB]);
+        var bom = newBillOfMaterials("myId");
+        parent.Insert(null, 1, [bom]);
+        Assert.AreSame(parent, docA.GetParent());
+        Assert.AreSame(parent, docB.GetParent());
+        Assert.AreSame(parent, bom.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(bom));
+        CollectionAssert.AreEqual(new List<INode> { docA, bom, docB }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void Single_Insert_Two_After()
+    {
+        var docA = newDocumentation("cIdA");
+        var docB = newDocumentation("cIdB");
+        var parent = newLine("g");
+        parent.Add(null, [docA, docB]);
+        var bom = newBillOfMaterials("myId");
+        parent.Insert(null, 2, [bom]);
+        Assert.AreSame(parent, docA.GetParent());
+        Assert.AreSame(parent, docB.GetParent());
+        Assert.AreSame(parent, bom.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(bom));
+        CollectionAssert.AreEqual(new List<INode> { docA, docB, bom }, parent.GetAnnotations().ToList());
+    }
+
+    #endregion
+
+    #region Remove
+
+    [TestMethod]
+    public void Single_Remove_Empty()
+    {
+        var parent = newLine("g");
+        var bom = newBillOfMaterials("myId");
+        parent.Remove(null, [bom]);
+        Assert.IsNull(bom.GetParent());
+        Assert.IsFalse(parent.GetAnnotations().Contains(bom));
+    }
+
+    [TestMethod]
+    public void Single_Remove_NotContained()
+    {
+        var doc = newDocumentation("myC");
+        var parent = newLine("cs");
+        parent.Add(null, [doc]);
+        var bom = newBillOfMaterials("myId");
+        parent.Remove(null, [bom]);
+        Assert.AreSame(parent, doc.GetParent());
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { doc }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void Single_Remove_Only()
+    {
+        var bom = newBillOfMaterials("myId");
+        var parent = newLine("g");
+        parent.Add(null, [bom]);
+        parent.Remove(null, [bom]);
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void Single_Remove_First()
+    {
+        var doc = newDocumentation("cId");
+        var bom = newBillOfMaterials("myId");
+        var parent = newLine("g");
+        parent.Add(null, [bom, doc]);
+        parent.Remove(null, [bom]);
+        Assert.AreSame(parent, doc.GetParent());
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { doc }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void Single_Remove_Last()
+    {
+        var doc = newDocumentation("cId");
+        var bom = newBillOfMaterials("myId");
+        var parent = newLine("g");
+        parent.Add(null, [doc, bom]);
+        parent.Remove(null, [bom]);
+        Assert.AreSame(parent, doc.GetParent());
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { doc }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void Single_Remove_Between()
+    {
+        var docA = newDocumentation("cIdA");
+        var docB = newDocumentation("cIdB");
+        var bom = newBillOfMaterials("myId");
+        var parent = newLine("g");
+        parent.Add(null, [docA, bom, docB]);
+        parent.Remove(null, [bom]);
+        Assert.AreSame(parent, docA.GetParent());
+        Assert.AreSame(parent, docB.GetParent());
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { docA, docB }, parent.GetAnnotations().ToList());
+    }
+
+    #endregion
+
+    #endregion
+
+    #region Null
+
+    // TODO: copy the rest of the tests from ContainmentTests_Annotation and replace api calls with generic calls 
 
     #endregion
 
