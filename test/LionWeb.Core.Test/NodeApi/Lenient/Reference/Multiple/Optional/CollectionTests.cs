1c1402b6fea971732baf4e810d6b0d84258cd548
Niko <github@nikostotz.de>
2025-11-26 23:02:19 +0100

    split up ReferenceTests_Multiple_Optional.cs (no content change)

---
+++ test/LionWeb.Core.Test/NodeApi/Lenient/Reference/Multiple/Optional/CollectionTests.cs
@@ -0,0 +1,259 @@
+﻿// Copyright 2024 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.Test.NodeApi.Lenient.Reference.Multiple.Optional;
+
+using System.Collections;
+
+[TestClass]
+public class CollectionTests : LenientNodeTestsBase
+{
+    #region EmptyCollection
+
+    [TestMethod]
+    public void EmptyArray_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new LenientNode[0];
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyUntypedList_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new ArrayList();
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyListMatchingType_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new List<LenientNode>();
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyListSubtype_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new List<LenientNode>();
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptySet_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new HashSet<LenientNode>();
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyListNonMatchingType_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new List<string>();
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyList_Reset_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        parent.Set(ReferenceGeometry_shapes, new List<LenientNode> { newLine("myId") });
+        var values = new List<LenientNode>();
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    #endregion
+
+    #region NullCollection
+
+    [TestMethod]
+    public void NullArray_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new LenientNode[] { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(ReferenceGeometry_shapes, values));
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullUntypedList_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new ArrayList() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(ReferenceGeometry_shapes, values));
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullListMatchingType_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new List<LenientNode>() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(ReferenceGeometry_shapes, values));
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullListSubtype_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new List<LenientNode>() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(ReferenceGeometry_shapes, values));
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullListNonMatchingType_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new List<string>() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(ReferenceGeometry_shapes, values));
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullSet_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var values = new HashSet<LenientNode>() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(ReferenceGeometry_shapes, values));
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    #endregion
+
+    #region SingleCollection
+
+    [TestMethod]
+    public void SingleArray_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newLine("s");
+        var values = new LenientNode[] { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleUntypedArray_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newLine("s");
+        var values = new object[] { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleUntypedList_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newLine("s");
+        var values = new ArrayList() { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleListMatchingType_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newLine("s");
+        var values = new List<LenientNode>() { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleListSubtype_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newLine("s");
+        var values = new List<LenientNode>() { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleSet_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newLine("s");
+        var values = new HashSet<LenientNode>() { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleListNonMatchingType_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newCoord("c");
+        var values = new List<LenientNode>() { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleUntypedListNonMatchingType_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newCoord("c");
+        var values = new ArrayList() { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleUntypedArrayNonMatchingType_Reflective()
+    {
+        var parent = newReferenceGeometry("g");
+        var value = newCoord("c");
+        var values = new object[] { value };
+        parent.Set(ReferenceGeometry_shapes, values);
+        Assert.IsNull(value.GetParent());
+        Assert.IsTrue((parent.Get(ReferenceGeometry_shapes) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    #endregion
+}
---
+++ test/LionWeb.Core.Test/NodeApi/Lenient/Reference/Multiple/Optional/MetamodelViolationTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2024 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.Test.NodeApi.Lenient.Reference.Multiple.Optional;
+
+[TestClass]
+public class MetamodelViolationTests : LenientNodeTestsBase
+{
+    [TestMethod]
+    public void String_Reflective()
+    {
+        var parent = newReferenceGeometry("od");
+        var value = "a";
+        parent.Set(ReferenceGeometry_shapes, value);
+        Assert.AreEqual("a", parent.Get(ReferenceGeometry_shapes));
+    }
+
+    [TestMethod]
+    public void Integer_Reflective()
+    {
+        var parent = newReferenceGeometry("od");
+        var value = -10;
+        parent.Set(ReferenceGeometry_shapes, value);
+        Assert.AreEqual(-10, parent.Get(ReferenceGeometry_shapes));
+    }
+}
