8a41c76df4ef5d5aa6f455f0a173b01989b7010c
Niko <github@nikostotz.de>
2025-11-26 19:57:20 +0100

    split up ContainmentTests_Annotation_GenericApi.cs (no content change)

---
+++ test/LionWeb.Core.Test/NodeApi/Generated/Annotation/GenericApi/CollectionTests.cs
@@ -0,0 +1,185 @@
+﻿// Copyright 2024 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.Test.NodeApi.Generated.Annotation.GenericApi;
+
+using LionWeb.Core.Test.Languages.Generated.V2024_1.Shapes.M2;
+
+[TestClass]
+public class CollectionTests
+{
+    #region EmptyCollection
+
+    [TestMethod]
+    public void EmptyArray()
+    {
+        var parent = new Line("g");
+        var values = new INode[0];
+        parent.Add(null, values);
+        Assert.IsTrue(parent.GetAnnotations().Count == 0);
+    }
+
+
+    [TestMethod]
+    public void Insert_EmptyArray()
+    {
+        var parent = new Line("g");
+        var values = new INode[0];
+        parent.Insert(null, 0, values);
+        Assert.IsTrue(parent.GetAnnotations().Count == 0);
+    }
+
+    [TestMethod]
+    public void Remove_EmptyArray()
+    {
+        var parent = new Line("g");
+        var values = new INode[0];
+        parent.Remove(null, values);
+        Assert.IsTrue(parent.GetAnnotations().Count == 0);
+    }
+
+    #endregion
+
+    #region NullCollection
+
+    [TestMethod]
+    public void NullArray()
+    {
+        var parent = new Line("g");
+        var values = new INode[] { null };
+        Assert.ThrowsExactly<InvalidValueException>(() => parent.Add(null, values));
+        Assert.IsTrue(parent.GetAnnotations().Count == 0);
+    }
+
+
+    [TestMethod]
+    public void Insert_NullArray()
+    {
+        var parent = new Line("g");
+        var values = new INode[] { null };
+        Assert.ThrowsExactly<InvalidValueException>(() => parent.Insert(null, 0, values));
+        Assert.IsTrue(parent.GetAnnotations().Count == 0);
+    }
+
+    [TestMethod]
+    public void Remove_NullArray()
+    {
+        var parent = new Line("g");
+        var values = new INode[] { null };
+        Assert.ThrowsExactly<InvalidValueException>(() => parent.Remove(null, values));
+        Assert.IsTrue(parent.GetAnnotations().Count == 0);
+    }
+
+    #endregion
+
+    #region SingleCollection
+
+    [TestMethod]
+    public void SingleArray()
+    {
+        var parent = new Line("g");
+        var value = new BillOfMaterials("s");
+        var values = new INode[] { value };
+        parent.Add(null, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(value));
+    }
+
+    [TestMethod]
+    public void Insert_SingleArray()
+    {
+        var parent = new Line("g");
+        var value = new BillOfMaterials("s");
+        var values = new INode[] { value };
+        parent.Insert(null, 0, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue(parent.GetAnnotations().Contains(value));
+    }
+
+    #region Remove
+
+    [TestMethod]
+    public void SingleArray_Remove_Empty()
+    {
+        var parent = new Line("g");
+        var bom = new BillOfMaterials("myId");
+        var values = new INode[] { bom };
+        parent.Remove(null, values);
+        Assert.IsNull(bom.GetParent());
+        Assert.IsFalse(parent.GetAnnotations().Contains(bom));
+    }
+
+    [TestMethod]
+    public void SingleArray_Remove_Only()
+    {
+        var bom = new BillOfMaterials("myId");
+        var parent = new Line("g");
+        parent.Add(null, [bom]);
+        var values = new INode[] { bom };
+        parent.Remove(null, values);
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void SingleArray_Remove_First()
+    {
+        var doc = new Documentation("cId");
+        var bom = new BillOfMaterials("myId");
+        var parent = new Line("g");
+        parent.Add(null, [bom, doc]);
+        var values = new INode[] { bom };
+        parent.Remove(null, values);
+        Assert.AreSame(parent, doc.GetParent());
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { doc }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void SingleArray_Remove_Last()
+    {
+        var doc = new Documentation("cId");
+        var bom = new BillOfMaterials("myId");
+        var parent = new Line("g");
+        parent.Add(null, [doc, bom]);
+        var values = new INode[] { bom };
+        parent.Remove(null, values);
+        Assert.AreSame(parent, doc.GetParent());
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { doc }, parent.GetAnnotations().ToList());
+    }
+
+    [TestMethod]
+    public void SingleArray_Remove_Between()
+    {
+        var docA = new Documentation("cIdA");
+        var docB = new Documentation("cIdB");
+        var bom = new BillOfMaterials("myId");
+        var parent = new Line("g");
+        parent.Add(null, [docA, bom, docB]);
+        var values = new INode[] { bom };
+        parent.Remove(null, values);
+        Assert.AreSame(parent, docA.GetParent());
+        Assert.AreSame(parent, docB.GetParent());
+        Assert.IsNull(bom.GetParent());
+        CollectionAssert.AreEqual(new List<INode> { docA, docB }, parent.GetAnnotations().ToList());
+    }
+
+    #endregion
+
+    #endregion
+}
