8fed2bea64374618a28d61e9ece6a9624c589ebc
Niko <github@nikostotz.de>
2025-11-26 22:51:18 +0100

    split up ContainmentTests_Multiple_Required.cs (no content change)

---
+++ test/LionWeb.Core.Test/NodeApi/Lenient/Containment/Multiple/Required/CollectionTests.cs
@@ -0,0 +1,289 @@
+﻿// Copyright 2024 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Core.Test.NodeApi.Lenient.Containment.Multiple.Required;
+
+using System.Collections;
+
+[TestClass]
+public class CollectionTests : LenientNodeTestsBase
+{
+    #region EmptyCollection
+
+    [TestMethod]
+    public void EmptyArray_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new LenientNode[0];
+        parent.Set(CompositeShape_parts, values);
+        Assert.ThrowsException<UnsetFeatureException>(
+            () => (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyUntypedList_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new ArrayList();
+        parent.Set(CompositeShape_parts, values);
+        Assert.ThrowsException<UnsetFeatureException>(
+            () => (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyListMatchingType_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new List<LenientNode>();
+        parent.Set(CompositeShape_parts, values);
+        Assert.ThrowsException<UnsetFeatureException>(
+            () => (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyListSubtype_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new List<INode>();
+        parent.Set(CompositeShape_parts, values);
+        Assert.ThrowsException<UnsetFeatureException>(
+            () => (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptySet_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new HashSet<LenientNode>();
+        parent.Set(CompositeShape_parts, values);
+        Assert.ThrowsException<UnsetFeatureException>(
+            () => (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyListNonMatchingType_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new List<string>();
+        parent.Set(CompositeShape_parts, values);
+        Assert.ThrowsException<UnsetFeatureException>(
+            () => (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void EmptyList_Reset_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newCircle("myId");
+        parent.Set(CompositeShape_parts, new List<LenientNode> { value });
+        var values = new List<LenientNode>();
+        parent.Set(CompositeShape_parts, values);
+        Assert.ThrowsException<UnsetFeatureException>(
+            () => (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    #endregion
+
+    #region NullCollection
+
+    [TestMethod]
+    public void NullArray_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new LenientNode[] { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(CompositeShape_parts, values));
+        Assert.ThrowsException<UnsetFeatureException>(() =>
+            (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullUntypedList_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new ArrayList() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(CompositeShape_parts, values));
+        Assert.ThrowsException<UnsetFeatureException>(() =>
+            (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullListMatchingType_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new List<LenientNode>() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(CompositeShape_parts, values));
+        Assert.ThrowsException<UnsetFeatureException>(() =>
+            (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullListSubtype_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new List<INode>() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(CompositeShape_parts, values));
+        Assert.ThrowsException<UnsetFeatureException>(() =>
+            (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullListNonMatchingType_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new List<string>() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(CompositeShape_parts, values));
+        Assert.ThrowsException<UnsetFeatureException>(() =>
+            (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void NullSet_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var values = new HashSet<LenientNode>() { null };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(CompositeShape_parts, values));
+        Assert.ThrowsException<UnsetFeatureException>(() =>
+            (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    #endregion
+
+    #region SingleCollection
+
+    [TestMethod]
+    public void SingleArray_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newLine("s");
+        var values = new LenientNode[] { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue((parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleArray_Existing_Reflective()
+    {
+        var circle = newCircle("cc");
+        var parent = newCompositeShape("g");
+        parent.Set(CompositeShape_parts, new LenientNode[] { circle });
+        var value = newLine("s");
+        var values = new LenientNode[] { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.IsNull(circle.GetParent());
+        Assert.AreSame(parent, value.GetParent());
+        CollectionAssert.AreEqual(new List<LenientNode> { value },
+            (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).ToList());
+    }
+
+    [TestMethod]
+    public void SingleUntypedArray_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newLine("s");
+        var values = new object[] { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue((parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleUntypedList_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newLine("s");
+        var values = new ArrayList() { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue((parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleListMatchingType_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newLine("s");
+        var values = new List<LenientNode>() { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue((parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleListSubtype_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newLine("s");
+        var values = new List<INode>() { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue((parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleSet_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newLine("s");
+        var values = new HashSet<LenientNode>() { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue((parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleListNonMatchingType_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = "c";
+        var values = new List<string>() { value };
+        Assert.ThrowsException<InvalidValueException>(
+            () => parent.Set(CompositeShape_parts, values));
+        Assert.ThrowsException<UnsetFeatureException>(() =>
+            (parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Count() == 0);
+    }
+
+    [TestMethod]
+    public void SingleUntypedListNonMatchingType_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newCoord("c");
+        var values = new ArrayList() { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue((parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    [TestMethod]
+    public void SingleUntypedArrayNonMatchingType_Reflective()
+    {
+        var parent = newCompositeShape("cs");
+        var value = newCoord("c");
+        var values = new object[] { value };
+        parent.Set(CompositeShape_parts, values);
+        Assert.AreSame(parent, value.GetParent());
+        Assert.IsTrue((parent.Get(CompositeShape_parts) as IEnumerable<IReadableNode>).Contains(value));
+    }
+
+    #endregion
+}
