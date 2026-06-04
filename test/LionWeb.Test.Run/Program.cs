ee0e8df6d8d7789e8b6459283e3c64a0666f8299
Niko <github@nikostotz.de>
2026-06-04 17:30:44 +0200

    added small cmdline executable for profiling instances

---
+++ test/LionWeb.Test.Run/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace LionWeb.Test.Run;
+
+using Core.Serialization;
+using Core.Test.Languages.Generated.V2024_1.TestLanguage;
+
+class Program
+{
+    private Dictionary<string, object>? _serializedNodes;
+
+    static void Main(string[] args)
+    {
+        var program = new Program();
+        program.SerializedNodes();
+
+        Console.WriteLine(program._serializedNodes?.Count);
+    }
+
+    public void SerializedNodes()
+    {
+        _serializedNodes = new SerializedNodeTest().CreateSerializedNodes();
+        foreach (var (key, value) in _serializedNodes)
+        {
+            Console.WriteLine($"{key}: {SizeOf(value)}");
+        }
+
+        for (int i = 0; i < 400; i++)
+        {
+            _serializedNodes[i.ToString()] = new SerializedNode()
+            {
+                Id = i.ToString(),
+                Classifier = TestLanguageLanguage.Instance.DataTypeTestConcept.ToMetaPointer(),
+                Properties = [],
+                Containments = [],
+                References = [],
+                Annotations = [],
+                Parent = null
+            };
+        }
+        
+        Thread.Sleep(5000);
+    }
+
+    private static int SizeOf(object obj)
+    {
+        unsafe
+        {
+            RuntimeTypeHandle th = obj.GetType().TypeHandle;
+            int size = *(*(int**)&th + 1);
+            return size;
+        }
+    }
+}
---
+++ test/LionWeb.Test.Run/SerializedNodeTest.cs
@@ -0,0 +1,63 @@
+// Copyright 2026 TRUMPF Laser SE and other contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace LionWeb.Test.Run;
+
+using Core.Serialization;
+
+public class SerializedNodeTest
+{
+    public Dictionary<string, object> CreateSerializedNodes() =>
+        new()
+        {
+            { "Serialized_Empty", new SerializedNode() { Id = null, Classifier = null } },
+            { "Serialized_Minimal", new SerializedNode() { Id = "myId", Classifier = Classifier() } },
+            {
+                "Serialized_Set", new SerializedNode()
+                {
+                    Id = "myId",
+                    Classifier = Classifier(),
+                    Properties = [],
+                    Containments = [],
+                    References = [],
+                    Annotations = []
+                }
+            },
+            { "Serialized_Annotation_Set", new SerializedNode() { Id = "myId", Classifier = Classifier(), Annotations = [] } },
+            { "Serialized_Annotation_Filled", new SerializedNode() { Id = "myId", Classifier = Classifier(), Annotations = ["ann"] } },
+            { "Property_Empty", new SerializedProperty { Property = null } },
+            { "Property_Minimal", new SerializedProperty { Property = Property() } },
+            { "Property_Filled", new SerializedProperty { Property = Property(), Value = "myProp" } },
+            { "Containment_Empty", new SerializedContainment() { Containment = null, Children = null } },
+            { "Containment_Minimal", new SerializedContainment() { Containment = Containment(), Children = null } },
+            { "Containment_Set", new SerializedContainment() { Containment = Containment(), Children = [] } },
+            { "Containment_Filled", new SerializedContainment() { Containment = Containment(), Children = ["childId"] } },
+            { "Reference_Empty", new SerializedReference() { Reference = null, Targets = null } },
+            { "Reference_Minimal", new SerializedReference() { Reference = Reference(), Targets = null } },
+            { "Reference_Set", new SerializedReference() { Reference = Reference(), Targets = [] } },
+            { "Reference_Filled", new SerializedReference() { Reference = Reference(), Targets = [new SerializedReferenceTarget() { Reference = "targetId" }] } },
+            { "Target_Empty", new  SerializedReferenceTarget() },
+            { "Target_Reference", new  SerializedReferenceTarget() {Reference = "ref"} },
+            { "Target_Info", new  SerializedReferenceTarget() {ResolveInfo = "ref text"} },
+            { "Target_Filled", new  SerializedReferenceTarget() {ResolveInfo = "ref text", Reference = "ref"} },
+        };
+
+    private static MetaPointer Classifier() => new("myLang", "0", "myKey");
+    private static MetaPointer Property() => new("myLang", "0", "myProp");
+    private static MetaPointer Containment() => new("myLang", "0", "myChild");
+    private static MetaPointer Reference() => new("myLang", "0", "myRef");
+}
