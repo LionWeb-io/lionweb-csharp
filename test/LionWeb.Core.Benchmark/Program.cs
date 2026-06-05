c7521abf9c854cbfee7fec0ad539ef7bd088c089
Luis Frey <luis@twobrains.com>
2026-06-05 15:07:23 +0200

    tmp: wasm benchmark

--- test/LionWeb.Core.Benchmark/Program.cs
+++ test/LionWeb.Core.Benchmark/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Copyright 2025 TRUMPF Laser SE and other contributors
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License")
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-FileCopyrightText: 2024 TRUMPF Laser SE and other contributors
 // SPDX-License-Identifier: Apache-2.0
 
@@ -21,6 +21,11 @@ using BenchmarkDotNet.Running;
 
 public class Program
 {
+    /*
+     * Setup Wasm tools:    `dotnet workload install wasm-tools`
+     * Run WASM benchmarks: `dotnet run -c Release -- --runtimes wasmnet10_0 --wasmEngine node`
+     * With AOT:            `dotnet run -c Release -- --runtimes wasmnet10_0 --wasmEngine node --AOTCompilerMode wasm`
+     */
     public static void Main(string[] args)
     {
         // new DeserializerBenchmark().Deserialize_StreamX_Aot();
